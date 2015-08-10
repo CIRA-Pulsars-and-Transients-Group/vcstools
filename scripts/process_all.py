9a71603d757583018986289f67db7c6e7febfe26
Stephen Ord <steve.ord@gmail.com>
2015-08-10 11:54:54 +0800

    process_all j argument fixed

--- scripts/process_all.py
+++ scripts/process_all.py
@@ -171,7 +171,7 @@ if __name__ == '__main__':
     the_options = {'begin': start_time, 'ncoarse_chan' : n_coarse, 'end' : stop_time, 'get_data':getdata, 'inc':increment,'useJones':useJones, 'mode': beam_mode, 'nchan':nchan, 'obsid': obsid, 'pointing' : pointing, 'single_step' : single_step, 'runPFB' : runPFB, 'runMWAC': runMWAC, 'corrdir': corrdir, 'Go':Go, 'runRECOMBINE' : runRECOMBINE, 'root' : working_root}
 
     try:
-        opts, args = getopt.getopt(sys.argv[1:],"hb:c:e:gGi:jm:n:o:p:r:Rs:w:z")
+        opts, args = getopt.getopt(sys.argv[1:],"hb:c:e:gGi:j:m:n:o:p:r:Rs:w:z")
     except getopt.GetoptError:
         usage(the_options)
         sys.exit()
