d6b1a0da6ea65d2ca7533b860d37b80b208e056d
Stephen Ord <steve.ord@gmail.com>
2015-08-19 14:05:51 +0800

    changed the working dir of the batch

--- scripts/process_all.py
+++ scripts/process_all.py
@@ -350,7 +350,7 @@ if __name__ == '__main__':
                     batch_line = "%s\n" % (get_data)
                     batch_file.write(batch_line)
             
-                submit_line = "sbatch --time=%s --workdir=%s -M zeus --partition=copyq %s\n" % (str(secs_to_run),working_dir,voltdownload_batch)
+                submit_line = "sbatch --time=%s --workdir=%s -M zeus --partition=copyq %s\n" % (str(secs_to_run),working_root,voltdownload_batch)
                 submit_cmd = subprocess.Popen(submit_line,shell=True,stdout=subprocess.PIPE)
                 continue
             else:
