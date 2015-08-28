9ca04af904f8f91232fee4b1b594b06a46d06d97
Steve Ord <steve.ord@gmail.com>
2015-08-28 13:33:15 +0800

    added good files countdown

--- scripts/recombine.py
+++ scripts/recombine.py
@@ -58,13 +58,13 @@ if __name__ == '__main__':
 
     time_to_combine = the_options['start']+rank
     files_glob = "%s/combined/%d_%d_ch*.dat" % (the_options['root'],the_options['obsid'],time_to_combine)
-    broken = 1;
+    broken = 24;
     for to_check in sorted(glob.glob(files_glob)):
         file_statinfo = os.stat(to_check)
         if (file_statinfo.st_size == testsize):
-            broken = 0
+            broken = broken-1
 
-    if (broken == 1):
+    if (broken > 0):
         f=[]
         for vcs in range(1,17):
             for stream in [0,1]:
