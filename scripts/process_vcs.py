b966b11f47123da0b192a67b420fb18674756838
stremblay <steven.tremblay@curtin.edu.au>
2015-11-04 14:51:45 +0800

    Added a normal chmod after folder creation, will see if we want sticky bit set later.

--- scripts/process_vcs.py
+++ scripts/process_vcs.py
@@ -54,7 +54,7 @@ def is_number(s):
 def mdir(path,description):
     try:
         os.mkdir(path)
-        os.fchmod(path,"g+s")
+        os.chmod(path,0661)
     except:
         if (os.path.exists(path)):
             print "{0} Directory Already Exists\n".format(description)
