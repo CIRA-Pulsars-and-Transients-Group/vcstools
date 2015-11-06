0ca64482e4d19e2aa97b31096719d6b0d62d2d8b
Stephen Ord <steve.ord@gmail.com>
2015-11-06 23:15:09 +0800

    Added one of the error macros that would have been in helper

--- gpu_utils/cuda_error.h
+++ gpu_utils/cuda_error.h
@@ -12,5 +12,19 @@ inline void gpuAssert( cudaError_t code, char * file, int line, bool abort=true)
 			exit( code );
 	}
 }
+#define getLastCudaError(msg)      __getLastCudaError (msg, __FILE__, __LINE__)
 
+inline void __getLastCudaError(const char *errorMessage, const char *file, const int line)
+{
+    cudaError_t err = cudaGetLastError();
+
+    if (cudaSuccess != err)
+    {
+        fprintf(stderr, "%s(%i) : getLastCudaError() CUDA error : %s : (%d) %s.\n",
+                file, line, errorMessage, (int)err, cudaGetErrorString(err));
+        exit(EXIT_FAILURE);
+    }
+}
 #endif
+
+
