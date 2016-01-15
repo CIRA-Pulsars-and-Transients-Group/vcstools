be70297826c0b0a32465e60e89362817799045bf
Franz Kirsten <fkirsten@galaxy-1.cm.cluster>
2016-01-15 12:05:21 +0800

    made things more robust and flexible

--- scripts/create_psrfits.sh
+++ scripts/create_psrfits.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 if [ $# -lt 1 ] ; then
-    echo "Usage: $0 obsID"  1>&2
+    echo "Usage: $0 obsID [base_directory]"  1>&2
     echo "    script will produce psrfits files in folder fits under obsID"  1>&2
+    echo "    base_directory is optional, default is /scratch/mwaops/vcs/obsID"  1>&2
+    echo "    if base_directory is provided script expects the combined folder to exist, will create fits folder."  1>&2
     exit 1
 fi
 
@@ -11,13 +13,14 @@ then
     rm /scratch/mwaops/vcs/$1/fits/mk_psrfits_in
 fi
 
+basedir=${2:-/scratch/mwaops/vcs/$1}
 
-length=`ls /scratch/mwaops/vcs/$1/combined/*ics.dat | wc -l`
-#echo "Length of Obs: $len"
+length=`ls ${basedir}/combined/*ics.dat | wc -l`
+#echo "Length of Obs: $length"
 
 #OLDIFS=$IFS
 #IFS='.'
-first_file=(`ls /scratch/mwaops/vcs/$1/combined/*ics.dat | head -n 1`)
+first_file=(`ls ${basedir}/combined/*ics.dat | head -n 1`)
 first_time=${first_file:(-18):10}
 #IFS=$' \t\n'
 
@@ -52,10 +55,10 @@ mjd=(${time_out[9]})
 mjd_whole=${mjd:0:5}
 #echo "Truncated MJD: $mjd_whole"
 
-mkdir /scratch/mwaops/vcs/$1/fits
-cd /scratch/mwaops/vcs/$1/fits
-echo -e "/scratch/mwaops/vcs/$1/fits/mk_psrfits_in\n\n$1\n\n${length}\n${USER}\n\n$1\n\n\nMWA-G0024\n${start}\n\n${freq}\n30.72\n${RA}\n${Dec}\n${Azimuth}\n${Zenith}\n\n${lst_s}\n${utc_s}\n${mjd_whole}\n\n\n\n\n\n\n\n\n" | make_psrfits & sleep 1.0
-cat /scratch/mwaops/vcs/$1/combined/*ics.dat > /scratch/mwaops/vcs/$1/fits/mk_psrfits_in
+mkdir ${basedir}/fits
+cd ${basedir}/fits
+echo -e "${basedir}/fits/mk_psrfits_in\n\n$1\n\n${length}\n${USER}\n\n$1\n\n\nMWA-G0024\n${start}\n\n${freq}\n30.72\n${RA}\n${Dec}\n${Azimuth}\n${Zenith}\n\n${lst_s}\n${utc_s}\n${mjd_whole}\n\n\n\n\n\n\n\n\n" | make_psrfits & sleep 1.0
+cat ${basedir}/combined/*ics.dat > ${basedir}/fits/mk_psrfits_in
 wait
 
 #IFS=$OLDIFS
