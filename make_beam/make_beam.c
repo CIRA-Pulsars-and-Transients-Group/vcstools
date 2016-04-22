985e360756c223dfc801a866dc0678fc77fbb493
steve-ord <steve.ord@gmail.com>
2016-04-22 22:42:08 +1000

    changed the offsets to be based upon mean value and not variance ...

--- make_beam/make_beam.c
+++ make_beam/make_beam.c
@@ -397,12 +397,13 @@ void flatten_bandpass(int nstep, int nchan, int npol, void *data, float *scales,
         
         for (j=0;j<nchan;j++){
             for (p = 0;p<npol;p++) {
-                *out = (chan_max[p][j] - chan_min[p][j])/127.0; // removed a divide by 32 here ....
+                // current mean
+                *out = (band[p][j]/nstep)/new_mean; // removed a divide by 32 here ....
               
                 
                 fprintf(stderr,"Channel %d pol %d mean: %f normaliser %f\n",j,p,(band[p][j]/nstep),*out);
                 out++;
-                *off = chan_min[p][j];
+                *off = (band[p][j]/nstep);
                 
                 off++;
                 
