6332244ee781bf2174aa61a83c8ba813c4bf73e2
Bradley Meyers <bradley.meyers1993@gmail.com>
2017-01-19 09:15:31 +0800

    Added conditional for if there is only 1 file created

--- concat.sh
+++ concat.sh
@@ -8,6 +8,13 @@ FNAME=$2 # name of output file
 NPROCESSES=$3 # total number of processes used = total number of data files
 
 # cat the data from each file into
-for i in {0..${NPROCESSES}};do cat ${BASENAME}.${i}.dat >> $FNAME;done
-
+n=`bc -l <<< "${NPROCESSES}-1"`
+if [ $n -eq 0 ]; then
+	echo "only 1 file created - just renaming..."
+	echo "mv ${BASENAME}.0.dat ${FNAME}"
+	mv ${BASENAME}.0.dat ${FNAME}
+else
+	echo "concatenating ${n} files together..."
+	for i in $(eval echo "{0..$n}");do echo "concatenating file $i"; cat ${BASENAME}.${i}.dat >> $FNAME;done
+fi
 
