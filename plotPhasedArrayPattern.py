d6f386dad7f88e404ff3055897082e30b328abb9
Bradley Meyers <bradley.meyers1993@gmail.com>
2017-01-19 11:18:29 +0800

    Re-wrote to be able to plot contours just from reading in the necessary columns (plt.tricontourf)

--- plotPhasedArrayPattern.py
+++ plotPhasedArrayPattern.py
@@ -6,8 +6,22 @@ import matplotlib.pyplot as plt
 import sys
 
 fname = sys.argv[1]
-table = np.genfromtxt(fname,comments="#",skip_header=14,usecols=(0,1,8),max_rows=1e6)
-theta = table[0]
-phi = table[1]
-power = table[2]
+print "loading data"
+theta,phi,beam = np.genfromtxt(fname,comments=("#","*"),skip_header=14,usecols=(0,1,8),unpack=True)
+print "done"
+
+print "plotting in polar coordinates"
+fig = plt.figure()
+ax = fig.add_subplot(111,polar=True)
+ax.tricontourf(np.radians(phi),np.radians(theta),beam)
+plt.savefig(fname.replace(".dat",".png"),bbox_inches='tight')
+
+#BASIC TEST TO MAKE SURE DATA ARE LOADED ETC
+#fig = plt.figure()
+#ax = fig.add_subplot(111,polar=True)
+#ax.scatter(theta,phi,c=beam,edgecolors='none')
+#plt.show()
+
+
+
 
