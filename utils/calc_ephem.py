5be8415951529aab4d31c0194939ba63beec955d
Bradley Meyers <bradley.meyers1993@gmail.com>
2017-04-07 13:18:31 +0800

    Added a utility script to calculate a 24hr ephemeris of a given target for some given observer position and date.

---
+++ utils/calc_ephem.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+
+"""
+Updated to be a little more general and not require LSL libraries...
+I suppose the next step would be to move over to using astropy given that pyephem is depricated now...
+
+Date: 2016/06/13
+Author: Bradley Meyers
+"""
+
+#import ephem
+#from lsl.common.stations import lwa1
+#from psr_constants import *
+#from math import pi
+import matplotlib.pyplot as plt
+import numpy as np
+from astropy.coordinates import EarthLocation,SkyCoord,AltAz
+from astropy import units as u
+from astropy.time import Time,TimezoneInfo
+from datetime import datetime
+import sys
+import argparse
+
+def calculate_ephem(ra,dec,date,tzoffset,lat=-26.7033,lon=116.671,elev=377.827):
+    
+    location = EarthLocation(lat=lat*u.deg, lon=lon*u.deg, height=elev*u.m)
+
+    target = SkyCoord(ra,dec,unit=(u.hourangle,u.deg))
+    
+    year,month,day = date.split("/")
+    hours = np.arange(0,24,0.01)
+    hr = [int(i) for i in hours]
+    mi = [int((hours[i]-hr[i])*60) for i in range(len(hours))]
+    se = [0]*len(hours)
+    t = [datetime(int(year),int(month),int(day),h,m,s) for h,m,s in zip(hr,mi,se)]
+ 
+    times = Time(t,scale='utc',format='datetime')
+    altaz = target.transform_to(AltAz(obstime=times,location=location))
+
+    alt = altaz.alt.deg
+ 
+    maxidx = np.where(alt==alt.max())[0][0]
+    maxtime = times[maxidx]
+    tz = TimezoneInfo(utc_offset=tzoffset*u.hour)
+    lst = str(maxtime.to_datetime(timezone=tz))
+    utcoff = lst[-6:]
+    lst = lst[:-6]
+    print "Local time (UTC{0}) of maximum elevation: {1}".format(utcoff,lst)
+
+    fig,ax = plt.subplots()
+    ax.plot(hours,alt,color='r',lw=2,alpha=0.6)
+    ax.axhline(0,ls="--",color='k')
+    ax.axvline(hours[maxidx],ls="--",color='r',lw=2)
+    ax.set_xlim(0,24)
+    ylims = ax.get_ylim()
+    if ylims[1] > 90:
+	ax.set_ylim(ylims[0],90)
+	
+    zero = np.zeros(len(alt))
+    ax.fill_between(hours,[ax.get_ylim()[0]]*len(hours),interpolate=True,color='gray')
+    ax.set_title("Source: {0} {1},\n max. elev: {2:.2f}deg @  {3} UTC{4}".format(ra,dec,alt.max(),lst,utcoff))
+    ax.set_xlabel("Time since UTC {0} 00:00:00  [hours]".format(date))
+    ax.set_ylabel("Elevation  [deg]")
+    plt.show()
+
+
+
+
+parser = argparse.ArgumentParser(description="Determine ephemeris for source object over 24hrs.")
+parser.add_argument('--ra',type=str,metavar="RAJ2000",help="RAJ2000 coordinate of target source (hh:mm:ss.ss)",required=True)
+parser.add_argument('--dec',type=str,metavar="DECJ2000",help="DECJ2000 coordinate of target source (dd:mm:ss.ss)",required=True)
+parser.add_argument('--utcdate',type=str,metavar="date",help="Desired ephemeris UTC date (YYYY/MM/DD)",required=True)
+parser.add_argument('--utcoff',type=int,metavar="offset",help="Hour offset from UTC [default = 0]",default=0)
+parser.add_argument('--observer',nargs=3,metavar=("lat","lon","elev"),help="Latitude (deg), longitude (deg) and elevation (m) of observer. Defaults to MWA.",default=(-26.7033,116.671,377.827))
+args = parser.parse_args()
+
+
+calculate_ephem(args.ra,args.dec,args.utcdate,args.utcoff,*args.observer)
+
