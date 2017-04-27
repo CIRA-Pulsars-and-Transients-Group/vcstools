Correct effective area calculation -- multiplied by a factor of 4pi

pabeam.py
@@ -293,10 +293,11 @@ def createArrayFactor(targetRA,targetDEC,obsid,delays,time,obsfreq,eff,flagged_t
 			array_factor /= len(xpos)
 			
 			# keep track of maximum value calculated
-			if array_factor > array_factor_max: array_factor_max = array_factor
+			if np.abs(array_factor)**2 > array_factor_max: array_factor_max = np.abs(array_factor)**2
 	
 			# calculate the tile beam at the given Az,ZA pixel
-			tile_xpol,tile_ypol = pb.MWA_Tile_full_EE([[za]],[[az]],freq=obsfreq,delays=[delays,delays],power=True,zenithnorm=True,interp=False)
+			#tile_xpol,tile_ypol = pb.MWA_Tile_full_EE([[za]],[[az]],freq=obsfreq,delays=[delays,delays],power=True,zenithnorm=True,interp=False)
+			tile_xpol,tile_ypol = pb.MWA_Tile_analytic(za,az,freq=obsfreq,delays=delays,power=True,zenithnorm=True)
 			tile_pattern = (tile_xpol+tile_ypol)/2.0
 			
 			# calculate the phased array power pattern 
@@ -329,7 +330,7 @@ def createArrayFactor(targetRA,targetDEC,obsid,delays,time,obsfreq,eff,flagged_t
 					# values are calculated using that convetion, so we need to represent that here
 				f.write("{0}\t{1}\t0\t0\t0\t0\t0\t0\t{2}\n".format(res[0],res[1],res[2]))
 		
-		print "worker {0} pattern maximum = {1}".format(rank,pattern_max)	
+		#print "worker {0} pattern maximum = {1}".format(rank,pattern_max)	
 	else:
 		print "worker {0} not writing".format(rank)
 		
@@ -340,32 +341,33 @@ def createArrayFactor(targetRA,targetDEC,obsid,delays,time,obsfreq,eff,flagged_t
 #####################
 ##  OPTION PARSING ##
 #####################
+
 parser = argparse.ArgumentParser(description="""Script to calculate the array factor required to model the tied-array beam for the MWA. 
 						This is an MPI-based simulation code and will use all available processes when run 
 						(i.e. there is no user choice in how many to use)""",\
-				formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+					formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
 parser.add_argument("-o","--obsid",type=str,action='store',metavar="obsID",\
-		help="""Observation ID (used to figure out spatial configuration of array). 
-			Also used to retrieve observation start-time and duration.""",default=None)
+			help="""Observation ID (used to figure out spatial configuration of array). 
+				Also used to retrieve observation start-time and duration.""",default=None)
 
 parser.add_argument("--flagged_tiles",type=str,nargs='+',action='store',metavar="tile",\
-		help="The tiles flagged as in when running the RTS. Must be a list of space separated tile numbers, e.g. 0 1 2 5",default=None)
+			help="The tiles flagged as in when running the RTS. Must be a list of space separated tile numbers, e.g. 0 1 2 5",default=None)
 
-parser.add_argument("-p","--target",type=str,nargs=2,action='store',metavar=("ra","dec"),\
-		help="The RA and DEC of the target pointing (i.e the desired phase-centre). Should be formtted like: hh:mm:ss.sss dd\"mm\'ss.ssss",\
-		default=("00:00:00.0000","00:00:00.0000"))
+parser.add_argument("-p","--target",nargs=2,metavar=("ra","dec"),\
+			help="The RA and DEC of the target pointing (i.e the desired phase-centre). Should be formtted like: hh:mm:ss.sss dd\"mm\'ss.ssss",\
+			default=("00:00:00.0000","00:00:00.0000"))
 
 parser.add_argument("-t","--time",type=float,action='store',metavar="time",\
-		help="""The GPS time desired for beam evaluation. This will override whatever is read from the metafits.""",default=None)
+			help="""The GPS time desired for beam evaluation. This will override whatever is read from the metafits.""",default=None)
 
 parser.add_argument("-f","--freq",type=float,action='store',metavar="frequency",help="The centre observing frequency for the observation (in Hz!)",default=184.96e6)
 
 parser.add_argument("-e","--efficiency",type=float,action='store',metavar="eta",help="Frequency and pointing dependent array efficiency",default=1)
 
 parser.add_argument("--grid_res",type=float,action='store',nargs=2,metavar=("theta_res","phi_res"),
-		help="""Resolution of the Azimuth (Az) and Zenith Angle (ZA) grid to be created in degrees. 
-			Be warned: setting these too small will result in a MemoryError and the job will die.""",default=(0.1,0.1))
+			help="""Resolution of the Azimuth (Az) and Zenith Angle (ZA) grid to be created in degrees. 
+				Be warned: setting these too small will result in a MemoryError and the job will die.""",default=(0.1,0.1))
 
 parser.add_argument("--coplanar",action='store_true',help="Assume the array is co-planar (i.e. height above array centre is 0 for all tiles)")
 
@@ -374,8 +376,8 @@ parser.add_argument("--zenith",action='store_true',help="Assume zenith pointing 
 parser.add_argument("--out_dir",type=str,action='store',help="Location (full path) to write the output data files",default=".")
 
 parser.add_argument("--write",action='store_true',
-		help="""Write the beam pattern to disk when done calculating. 
-			If this option is not passed, you will just get a '.stats' files containing basic information about simulation parameters and the calculated gain.""")
+			help="""Write the beam pattern to disk when done calculating. 
+				If this option is not passed, you will just get a '.stats' files containing basic information about simulation parameters and the calculated gain.""")
 
 # parse the arguments
 args = parser.parse_args()
@@ -388,7 +390,8 @@ rank = comm.Get_rank()
 size = comm.Get_size()
 
 # small calculations and data gethering from arguments is fine and won't run into trouble by having multiple processes do it simultaneously
-ra,dec = args.target
+ra = str(args.target[0]).replace('"','').replace("'","")
+dec = args.target[1].replace('"','').replace("'","")
 tres,pres = args.grid_res
 ntheta,nphi = 90/tres,360/pres
 
@@ -482,8 +485,8 @@ comm.barrier()
 
 # calculate the gain for that pointing and frequency and write a "stats" file
 if rank == 0:
-	eff_area = args.efficiency*((c.value/args.freq)**2/result)
-	gain = (1e-26)*eff_area/(2*k_B.value)
+	eff_area = args.efficiency * (c.value / args.freq)**2 * (4 * np.pi / result)
+	gain = (1e-26) * eff_area / (2 * k_B.value)
 	
 	if args.write:
 		nfiles = size