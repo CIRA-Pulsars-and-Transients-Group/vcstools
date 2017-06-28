9bc34061e9f0075e6bdd53d548b10d2852347cb6
Sam McSweeney <sammy.mcsweeney@gmail.com>
2017-06-28 11:13:51 +0800

    Started the work of cleaning up and minimalising the beamformer. This version is just a pared-down version of the original, but saved in a separate source file, so the original is untouched. It produces the same output, but only for fits files. For now, I'm not worrying about the vdif output.

--- test/timing_test.sh
+++
@@ -1,7 +0,0 @@
-cd ./01
-
-pam -e Fp -Fp coherent.ar
-pam --site 7 -m coherent.Fp
-pat -s ../coherent.std coherent.Fp -f \"tempo2\" > coherent.tim
-tempo2 -f ../coherent.par -gr plk coherent.tim
-cd ..
