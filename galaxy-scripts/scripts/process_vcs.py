3cac2a5bbc3e707be01078159138203c50d9aa94
Franz Kirsten <fkirsten@galaxy-1.pawsey.org.au>
2017-08-22 21:15:08 +0800

    made sure checks.py runs in workq on zeus instead of copyq, recombine checks.py jobs still run on gpuq on galaxy

--- galaxy-scripts/scripts/process_vcs.py
+++ galaxy-scripts/scripts/process_vcs.py
@@ -292,7 +292,7 @@ def vcs_download(obsid, start_time, stop_time, increment, head, data_dir, produc
                             commands.append("else")
                             commands.append("sbatch {0}.batch".format(batch_dir+tar_batch))
                         commands.append("fi")
-                        submit_slurm(check_batch,commands,batch_dir=batch_dir, slurm_kwargs={"time" : check_secs_to_run, "partition" : "copyq", "clusters":"zeus"}, submit=False, outfile=batch_dir+check_batch+"_0.out", cluster="zeus")
+                        submit_slurm(check_batch,commands,batch_dir=batch_dir, slurm_kwargs={"time" : check_secs_to_run, "partition" : "workq", "clusters":"zeus"}, submit=False, outfile=batch_dir+check_batch+"_0.out", cluster="zeus")
                         
                         # Write out the tar batch file if in mode 15
                         #if format == 16:
