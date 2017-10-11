9724d3ab0fd9347800271ca03463f11a42564046
Bradley Meyers <bradley.meyers1993@gmail.com>
2017-10-11 14:15:14 +0800

    Cleaned up gather_gains script

--- gather_gains.sh
+++ gather_gains.sh
@@ -3,9 +3,10 @@
 obsid=$1
 
 for s in ${obsid}*.stats;do
-	gps=${s:11:10}
-	freq=${s:24:6}
-	gain=`sed -n '/#gain/{n;p;}' $s`
+    gps=${s:11:10}
+    freq=${s%%MHz*}
+    freq=${freq##*_}
+    gain=`sed -n '/#gain/{n;p;}' $s`
 
-	echo $gps $gain >> ${1}_${freq}.gains
+    echo $gps $gain >> ${1}_${freq}MHz.gains
 done 
