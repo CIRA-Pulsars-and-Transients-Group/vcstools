4067e110bedfa6671c571fc22f832406f8f04ffd
Bradley Meyers <bradley.meyers1993@gmail.com>
2018-02-27 10:21:07 +0800

    Corrected srun command to use right flags and use the rts_gpu executable

--- galaxy-scripts/scripts/calibrate_vcs.py
+++ galaxy-scripts/scripts/calibrate_vcs.py
@@ -661,10 +661,10 @@ class RTScal(object):
         nnodes = 25  # number of required GPU nodes - 1 per coarse channels + 1 master node
         rts_batch = "RTS_{0}".format(self.cal_obsid)
         slurm_kwargs = {"partition": "gpuq", "workdir": "{0}".format(self.rts_out_dir), "time": "00:20:00",
-                        "nodes": "{0}".format(nnodes), "gres": "gpu:1"}
+                "nodes": "{0}".format(nnodes), "gres": "gpu:1", "ntasks-per-node": "1"}
         commands = list(self.script_body)  # make a copy of body to then extend
-        #commands.append("srun -n {0} -c 1  rts_gpu {1}".format(nnodes, fname))
-        commands.append("srun rts_cpu {0}".format(fname))
+        commands.append("srun -N {0} -n {0}  rts_gpu {1}".format(nnodes, fname))
+        #commands.append("srun rts_cpu {0}".format(fname))
         jobid = submit_slurm(rts_batch, commands, slurm_kwargs=slurm_kwargs, batch_dir=self.batch_dir,
                              submit=self.submit)
         jobids.append(jobid)
@@ -839,10 +839,10 @@ class RTScal(object):
             chans = k.split('_')[-1].split(".")[0]
             rts_batch = "RTS_{0}_{1}".format(self.cal_obsid, chans)
             slurm_kwargs = {"partition": "gpuq", "workdir": "{0}".format(self.rts_out_dir), "time": "00:45:00",
-                            "nodes": "{0}".format(nnodes), "gres": "gpu:1"}
+                    "nodes": "{0}".format(nnodes), "gres": "gpu:1", "ntasks-per-node": "1"}
             commands = list(self.script_body)  # make a copy of body to then extend
-            #commands.append("srun -n {0} -c 1  rts_gpu {1}".format(nnodes, k))
-            commands.append("srun rts_cpu {0}".format(k))
+            commands.append("srun -N {0} -n {0}  rts_gpu {1}".format(nnodes, k))
+            #commands.append("srun rts_cpu {0}".format(k))
             jobid = submit_slurm(rts_batch, commands, slurm_kwargs=slurm_kwargs, batch_dir=self.batch_dir,
                                  submit=self.submit)
             jobids.append(jobid)
