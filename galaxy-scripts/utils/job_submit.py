ae542595ed8e0f765cb651e59418e85e67c992b0
Bradley Meyers <bradley.meyers1993@gmail.com>
2018-02-28 14:08:18 +0800

    Significantly expanded upon function description in a more PEP8/numpy format.

--- galaxy-scripts/utils/job_submit.py
+++ galaxy-scripts/utils/job_submit.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-TMPL = """#!/bin/bash -l
+SLURM_TMPL = """#!/bin/bash -l
 
 #SBATCH --export={export}
 #SBATCH --output={outfile}
@@ -13,14 +13,60 @@ TMPL = """#!/bin/bash -l
 {script}
 """
 
-def submit_slurm(name, commands, slurm_kwargs={}, tmpl=TMPL, 
+def submit_slurm(name, commands, tmpl=SLURM_TMPL, slurm_kwargs={}, 
                     batch_dir="batch/", depend=None, submit=True, 
                     outfile=None, cluster="galaxy", export="NONE"):
     """
     Making this function to cleanly submit slurm jobs using a simple template.
-    This will use the <name> to setup both the .batch and .out files into <batch_dir>
-    <slurm_kwargs> should be a dictionary of keyword, value pairs of anything the template header is missing
-    <commands> is the actual batch script you want to run, and is expecting a list with one entry per line
+
+    Parameters
+    ----------
+    name : str
+        The base name that is used to create the "`name.batch`" and "`name.out`" files.
+
+    commands : list of strs
+        The actual bash script commands you wnat to run. 
+        Expects a list where each element is a single line of the bash script.
+
+    tmpl : str
+        A template header string with format place holders: export, outfile, cluster, header and script.
+        This is used to create the final string to be written to the job script `outfile`.
+        For this function, it is required to be SLURM compliant. 
+        Default: `SLURM_TMPL`
+
+    slurm_kwargs : dict [optional]
+        A dictionary of SLURM keyword, value pairs to fill in whatever is not in the template supplied to `tmpl`.
+        Default: `{}` (empty dictionary, i.e. no additional header parameters)
+
+    batch_dir : str [optional]
+        The LOCAL directory where you want to write the batch scripts (i.e. it will write to `$PWD/batch_dir`).
+        Default: "`batch/`"
+
+    depend : int or None [optional]
+        The job ID that the desired job depends on to finish with the "afterok" qualifier from SLURM.
+        If `None` then it is assumed there is no dependency on any other job.
+        Default: `None`
+
+    submit : boolean [optional]
+        Whether to write and submit the job scripts (`True`) or only write the scripts (`False`).
+        Default: `True`
+
+    outfile : str [optional]
+        The output file name if "`name.out`" is not desirable.
+        Default: `None` (i.e. "`batch_dir/name.out`")
+
+    cluster : str [optional]
+        The compute cluster that the job is to run on.
+        Default: "`galaxy`"
+
+    export : str [optional]
+        Switch that lets SLURM use your login environment on the compute nodes ("`ALL`") or not ("`NONE`").
+        Default: "`NONE`"
+
+    Returns
+    -------
+    jobid : int
+        The unique SLURM job ID associated with the submitted job.
     """
 
     header = []
