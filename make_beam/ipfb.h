83c6ffc61438637735acfee4095a749f268724af
Sam McSweeney <sammy.mcsweeney@gmail.com>
2018-05-15 15:21:32 +0800

    Now the host and device memory needed for the inverse PFB kernel is only allocated and freed once. Also, the filter coefficients are only loaded once into GPU memory.

--- make_beam/ipfb.h
+++ make_beam/ipfb.h
@@ -4,9 +4,29 @@
 #include <cuda_runtime.h>
 #include "mycomplex.h"
 
+struct gpu_ipfb_arrays
+{
+    int ntaps;
+    int in_size;
+    int fils_size;
+    int out_size;
+    float *in_real,     *in_imag;
+    float *d_in_real,   *d_in_imag;
+    float *fils_real,   *fils_imag;
+    float *d_fils_real, *d_fils_imag;
+    float *d_out;
+};
+
 void cu_invert_pfb_ord( ComplexDouble ***detected_beam, int file_no,
                         int nsamples, int nchan, int npol,
-                        ComplexDouble **fils, int fil_size,
-                        float *data_buffer_uvdif );
+                        struct gpu_ipfb_arrays *g, float *data_buffer_uvdif );
+
+void cu_load_filter( ComplexDouble **fils, struct gpu_ipfb_arrays *g,
+        int nchan );
+
+void malloc_ipfb( struct gpu_ipfb_arrays *g, int ntaps, int nsamples,
+        int nchan, int npol, int fil_size );
+
+void free_ipfb( struct gpu_ipfb_arrays *g );
 
 #endif
