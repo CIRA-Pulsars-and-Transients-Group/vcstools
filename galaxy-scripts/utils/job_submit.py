e9594490cfa513111c588a8afcab43606b04a13e
NickSwainston <nickaswainston@gmail.com>
2018-07-13 09:46:11 +0800

    Made the jobid catcdhing more robust so that if there's an error submitting the script doesn't fail

--- galaxy-scripts/utils/job_submit.py
+++ galaxy-scripts/utils/job_submit.py
@@ -106,10 +106,17 @@ def submit_slurm(name, commands, tmpl=SLURM_TMPL, slurm_kwargs={},
 
     # submit the jobs
     batch_submit_line = "sbatch {0}".format(jobfile)
+    jobid = None
     if submit:
         submit_cmd = subprocess.Popen(batch_submit_line, shell=True, stdout=subprocess.PIPE)
         for line in submit_cmd.stdout:
             if "Submitted" in line:
                 jobid = str(line.split(" ")[3])
-
-    return jobid
+        if jobid is None:
+            print batch_submit_line
+            print submit_cmd.stdout
+            return
+        else:
+            return jobid
+    else:
+        return
