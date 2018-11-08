9f34991a9346a71d639061f0b8a2a0f6c92c35ca
NickSwainston <nickaswainston@gmail.com>
2018-11-08 11:04:51 +0800

    Rewrote most of the help text to make how to use the script cleare

--- database/submit_to_database.py
+++ database/submit_to_database.py
@@ -25,6 +25,7 @@ import matplotlib.pyplot as plt
 import glob
 from astropy.table import Table
 from astropy.time import Time
+import textwrap as _textwrap
 
 import ephem
 from mwapy import ephem_utils
@@ -42,6 +43,11 @@ import find_pulsar_in_obs as fpio
 
 web_address = 'mwa-pawsey-volt01.pawsey.org.au'
 auth = ('mwapulsar','veovys9OUTY=')
+
+class LineWrapRawTextHelpFormatter(argparse.RawDescriptionHelpFormatter):
+    def _split_lines(self, text, width):
+        text = _textwrap.dedent(self._whitespace_matcher.sub(' ', text).strip())
+        return _textwrap.wrap(text, width)
 
 def sex2deg( ra, dec):
     """
@@ -115,14 +121,6 @@ def get_Trec(tab,obsfreq):
         print "ERROR getting Trec"
     return Trec
 
-
-class SmartFormatter(argparse.HelpFormatter):
-
-    def _split_lines(self, text, width):
-        if text.startswith('R|'):
-            return text[2:].splitlines()  
-        # this is the RawTextHelpFormatter._split_lines
-        return argparse.HelpFormatter._split_lines(self, text, width)
 
 
 def sigmaClip(data, alpha=3, tol=0.1, ntrials=10):
@@ -498,44 +496,46 @@ def flux_cal_and_sumbit(time_detection, time_obs, metadata, bestprof_data,
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="""
-    This code is used to submit observations to the MWA Pulsar Database. The goal is to calculate all need values (flux density, width, scattering) for each observation and  submit them to the pulsar database without having to manually input them. It can also submit diagnostic files such as pulse profiles and calibrations. This code assumes that the observation is coherent and correlated using the RTS so please use --incoh and --andre if your observation is incoherent or correlated using Andre's Tools respectively.
-    Two common uses are to simply upload a tarred calibration file (this can be done before a detection is uploaded)\n
-    > submit_to_database.py -c <calibration tar file location> -o <obsid> --cal_id <calibrator obsid>\n
-    Another is to upload the pulsars parameters, such as flux density and width, to the database. To do this the bestprof file is needed \n
-    > submit_to_database.py -b <bestprof file location>\n
-    An example of uploading a diagnostic file is:
-    > submit_to_database.py  -o <obsid> -p <pulsar> --ppps <PRESTO prepfold output post script file location> --cal_id <calid>
-    """, formatter_class=SmartFormatter)
-    parser.add_argument('-o','--obsid',type=str,help='The obsid.')
-    parser.add_argument('--incoh',action='store_true',help='Used for incoherent detections to accurately calculate gain. Default is coherent.')
-    parser.add_argument('--andre',action='store_true',help="Used for calibrations done using Andre Offrina's tools. Default is RTS.")
+    parser = argparse.ArgumentParser(formatter_class=LineWrapRawTextHelpFormatter, description=_textwrap.dedent("""
+    This code is used to submit pulsar detections and observation calibrations to the MWA Pulsar Database. It can calculate all need values (flux density, width, scattering) for each detection and submit them to the pulsar database without having to manually input them. It can also submit diagnostic files such as pulse profiles and calibrations. This code assumes that the observation is coherent and correlated using the RTS so please use --incoh and --andre if your observation is incoherent or correlated using Andre's Tools respectively.
+
+    A common use case is to simply upload a calibration file (this can be done before a detection is uploaded)
+    > submit_to_database.py --cal_dir_to_tar <calibration directory> --srclist <srclist> -o <obs ID> -O <calibrator obs ID>
+    Another is to record a pulsar detection and its paramters, such as flux density and pulse width, to the database. To do this the bestprof file is needed 
+    > submit_to_database.py -b <bestprof file location>
+    Diagnostic files (see Upload Options) such as PRESTO plots can be uploaded using:
+    > submit_to_database.py  -o <obs ID> -O <calibrator obs ID> -p <pulsar> --ppps <PRESTO prepfold output post script file location>
+    The diagnostic files can be created using the Dspsr Calculation Options (not robustly tested).
+    """))
+    parser.add_argument('-o','--obsid',type=str,help='The observation ID (eg. 1221399680).')
+    parser.add_argument('-O', '--cal_id',type=str,help='The observation ID of the calibrator.')
     parser.add_argument('-p','--pulsar',type=str,help='The pulsar J name.')
-    parser.add_argument('-f','--fits_files',type=str,help='The fits files location to be used in any Detection Calculation processing. Recommended to end in *.fits and surrounded by quotation marks.', default = "/group/mwaops/vcs/${obsid}/fits/*fits")
-    parser.add_argument('--cal_id',type=str,help='The obsid of the calibrator.')
+    parser.add_argument('--incoh',action='store_true',help='Used for incoherent detections to accurately calculate gain. The default is coherent.')
+    parser.add_argument('--andre',action='store_true',help="Used for calibrations done using Andre Offrina's tools. Default is RTS.")
+        
+    calcargs = parser.add_argument_group('Detection Calculation Options', 'All the values of a pulsar detection (such as flux density, width, scattering) can be calculated by this code using either a .besprof file or a soon to be implemented DSPSR equivalent and automatically uploaded to the MWA Pulsar Database. Analysis files can still be uploaded before this step but this will leave the pulsar values as nulls.')
+    calcargs.add_argument('-b','--bestprof',type=str,help='The location of the .bestprof file. Using this option will cause the code to calculate the needed parameters to be uploaded to the database (such as flux density, width and scattering). Using this option can be used instead of inputting the observation ID and pulsar name.')
+    calcargs.add_argument('--start',type=str,help="The start time of the detection in seconds. For example if the detection begins at the start of the observation you would use --start 0. If this option isn't used, the code will calculate when the pulsar entered the beam", default = None)
+    calcargs.add_argument('--stop',type=str,help="The stop time of the detection in seconds. For example if the detection ended 60 minutes in use --end 600. If this option isn't used, the code will calculate when the pulsar exited the beam", default = None)
+    calcargs.add_argument('--trcvr',type=str, default = "/group/mwaops/PULSAR/MWA_Trcvr_tile_56.csv", help='File location of the receiver temperatures to be used. Only required if you do not want to use the default values located in %(default)s.')
 
-    calcargs = parser.add_argument_group('Detection Calculation Options', 'All the values of a pulsar detection for the MWA pulsar databse can be calculated by this code using either a .besprof file or a (Soon to be implimented dspsr equivalent). If neither are included then all values will be left as null and the observation(/detection) can still be used to upload files without the calculation being performed')
-    calcargs.add_argument('-b','--bestprof',type=str,help='The location of the .bestprof file. Using this option will cause the code to calculate the needed paramters to be uploaded to the database (such as flux density, width and scattering). Using this option can be used instead of inputing the obsid and pulsar.')
-    calcargs.add_argument('--start',type=str,help='The start time of the detection in seconds. For example: 0', default = None)
-    calcargs.add_argument('--stop',type=str,help='The stop time of the detection in seconds. For example: 1000', default = None)
-    calcargs.add_argument('--trcvr',type=str,help='File location of the reciever temperatures to be used',default = "/group/mwaops/PULSAR/MWA_Trcvr_tile_56.csv")
-
-    uploadargs = parser.add_argument_group('Upload options', 'The different options for each file type that can be uploaded to the pulsar database. Will cause an error if the wrong file type is being uploaded.')
-    uploadargs.add_argument('-a','--archive',type=str,help="The dspsr archive file location to be uploaded to the database. Expects a single file that is the output of dspsr using the pulsar's ephemeris.")
-    uploadargs.add_argument('--single_pulse_series',type=str,help='The single pulse series file location to be uploaded to the database. Expects a single file that is the output of dspsr in single pulse mode (the -s option).')
-    uploadargs.add_argument('--ppps',type=str,help="The Presto Prepfold PostScript file location to be uploaded to the database.")
-    uploadargs.add_argument('-i','--ippd',type=str,help="The Intergrates Pulse Profile given by Dspsr.")
-    uploadargs.add_argument('-w','--waterfall',type=str,help="A waterfall plot of pulse phase vs frequency using dspsr's psrplot.")
+    uploadargs = parser.add_argument_group('Upload Options', 'The different options for each file type that can be uploaded to the pulsar database. Will cause an error if the wrong file type is being uploaded.')
+    uploadargs.add_argument('--cal_dir_to_tar',type=str,help='The calibration directory of a calibration solution that you would like to tar and upload to the database (eg. /group/mwaops/vcs/1221832280/cal/1221831856/). Must be used with --srclist so the correct source list is uploaded. If the calibration files are in the default positions then they will be tared and uploaded.')
+    uploadargs.add_argument('--srclist',type=str,help='Used with --cal_dir to indicate the source list file location. eg /group/mwaops/vcs/1221832280/cal/1221831856/vis/srclist_pumav3_EoR0aegean_EoR1pietro+ForA_1221831856_patch1000.txt.')
     uploadargs.add_argument('-c','--calibration',type=str,help='The calibration solution file location to be uploaded to the database. Expects a single file so please zip or tar up the bandpass calibrations, the DI Jones matrices, the flagged_channels.txt file, the flagged_tiles.txt file, the rts.in file and the source file.')
-    uploadargs.add_argument('--cal_dir',type=str,help='The calibration directory (eg. /group/mwaops/vcs/1221832280/cal/1221831856/). Must be used with --srclist so the correct sourcelist is uploaded. If the calibaration files are in the default positions then they will be tared and uploaded.')
-    uploadargs.add_argument('--srclist',type=str,help='Used with --cal_dir to indicate the sourcelist file location. eg /group/mwaops/vcs/1221832280/cal/1221831856/vis/srclist_pumav3_EoR0aegean_EoR1pietro+ForA_1221831856_patch1000.txt.')
-
-    dspsrargs = parser.add_argument_group('Dspsr Calculation Options', "Requires the --fits_files. These options will send off dspsr jobs to process the needed files that can be uploaded to the database. The files will be uploaded automatically when the dspsr scripts are tested more") #TODO remove when I'm confident with dspsr
-    dspsrargs.add_argument('--u_archive', action='store_true',help='The archive to be processed with dspsr and then uploaded to the database')
-    dspsrargs.add_argument('--u_single_pulse_series', action='store_true',help='The single pulse series to be processed with dspsr and then uploaded to the database')
-    dspsrargs.add_argument('--u_ppps', action='store_true', help="The Presto Prepfold PostScript file will be process by sending off PRESTO jobs.")
-    dspsrargs.add_argument('--u_ippd', action='store_true', help="The Intergrates Pulse Profile file will be process by sending off DSPSR jobs.")
-    dspsrargs.add_argument('--u_waterfall', action='store_true', help="A waterfall plot of pulse phase vs frequency file will be process by sending off DSPSR jobs.")
+    uploadargs.add_argument('-a','--archive',type=str,help="The DSPSR archive file location to be uploaded to the database. Expects a single file that is the output of DSPSR using the pulsar's ephemeris.")
+    uploadargs.add_argument('--single_pulse_series',type=str,help='The single pulse series file location to be uploaded to the database. Expects a single file that is the output of DSPSR in single pulse mode (the -s option).')
+    uploadargs.add_argument('--ppps',type=str,help="The Presto Prepfold PostScript file location to be uploaded to the database. Expects a single file that is the output of PRESTO's prepfold script.")
+    uploadargs.add_argument('-i','--ippd',type=str,help="The Intergrated Pulse Profile (sometimes called a pulse profile) file location. Expects a single file that is the output of DSPSR's pav script.")
+    uploadargs.add_argument('-w','--waterfall',type=str,help="The file location of a waterfall plot of pulse phase vs frequency. Expects a single file that is the output of DSPSR's psrplot.")
+        
+    dspsrargs = parser.add_argument_group('DSPSR Calculation Options', "Requires the --fits_files option. These options are all boolean flags that when used will send off DSPSR jobs to process the needed files that can be uploaded to the database. The files will be uploaded automatically when the DSPS scripts are tested more") #TODO remove when I'm confident with dspsr
+    dspsrargs.add_argument('-f','--fits_files',type=str,help='The fits files location to be used in any Detection Calculation processing. Recommended to end in *.fits and surrounded by quotation marks.', default = "/group/mwaops/vcs/${obsid}/fits/*fits")
+    dspsrargs.add_argument('--u_archive', action='store_true',help='Used to create an archive file using DSPSR.')
+    dspsrargs.add_argument('--u_single_pulse_series', action='store_true',help='Used to create a single pulse archive using DSPSR.')
+    dspsrargs.add_argument('--u_ppps', action='store_true', help="Used to create a Presto Prepfold PostScript file using PRESTO")
+    dspsrargs.add_argument('--u_ippd', action='store_true', help="Used to create an Intergrated Pulse Profile (sometimes called a pulse profile) using DSPSR")
+    dspsrargs.add_argument('--u_waterfall', action='store_true', help="Used to create a waterfall plot of pulse phase vs frequency using DSPSR.")
     args=parser.parse_args()
 
 
@@ -626,11 +626,11 @@ if __name__ == "__main__":
                             pul_ra, pul_dec, incoh,
                             start = args.start, stop = args.stop, trcvr = args.trcvr)
 
-    if args.cal_dir:
+    if args.cal_dir_to_tar:
         if not args.srclist:
             print "You must use --srclist to define the srclist file location. Exiting"
             quit()
-        args.calibration = zip_calibration_files(args.cal_dir, args.cal_id, args.srclist)
+        args.calibration = zip_calibration_files(args.cal_dir_to_tar, args.cal_id, args.srclist)
 
 
     if args.pulsar and not args.bestprof:  
@@ -837,12 +837,4 @@ if __name__ == "__main__":
                               batch_dir="./",
                               slurm_kwargs={"time": "6:50:00", "partition": "workq"},
                               submit=True)
-    """
-    Program tests:
-    python submit_to_database.py  ../1120799848/fold/1120799848_PSR_0837-4135.pfd.bestprof
-    python submit_to_database.py --bestprof /group/mwaops/incoh_census_psr/PSR_Prof/1121173352nsch_PSR_1534-5334.pfd.bestprof  --u_archive --u_single_pulse_series --u_diagnostics
-    python submit_to_database.py --bestprof ../PSR_Prof/1152636328_PSR_1943-1237.pfd.bestprof  --u_archive --u_single_pulse_series --u_diagnostics --incoh
-    #^ no fits
-    python submit_to_database.py --bestprof ../PSR_Prof/1139324488_PSR_0837+0610.pfd.bestprof --u_archive --u_single_pulse_series --u_diagnostics --incoh
-
-    """
+    
