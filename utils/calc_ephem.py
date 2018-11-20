0a96439b83fd0cf9ff8da5d370645e12678465cf
Bradley Meyers <bradley.meyers1993@gmail.com>
2018-11-20 09:56:11 +0800

    Moved to using logging module. Clean up of global dictionaries and options

--- utils/calc_ephem.py
+++ utils/calc_ephem.py
@@ -1,10 +1,6 @@
 #!/usr/bin/env python
 
 """
-Updated to be a little more general and not require LSL libraries...
-I suppose the next step would be to move over to using astropy given that pyephem is depricated now...
- - BWM 2016/06/13
-
 This script will accept a source position and a UTC day in which to calculate the ephemerides for 24 hours.
 This can be done for multiple observatories simultaneously. 
 
@@ -22,40 +18,34 @@ from astropy.coordinates import get_body
 from astropy import units as u
 from astropy.time import Time, TimezoneInfo
 from datetime import datetime
+import logging
 
+
+logger = logging.getLogger(__name__)
 
 # Radio telescope dictionary, formatted as:
 #       Name:(latitude, longitude, elevation)
-site_dict = {
-"MWA":         (-26.7033, 116.671, 377.827),
-"PKS":         (-32.999944, 148.262306, 372),
-"ATCA":        (-30.312778, 149.550278, 209),
-"ASKAP":       (-26.696, 116.637, 372),
-"MOST":        (-35.370707, 149.424658, 737),
-"GMRT":        (19.096517, 74.049742, 656),
-"LWA":         (34.07, -107.63, 2124),
-"GBT":         (38.4331, -79.8397, 807),
-"VLA":         (34.078749, -107.618283, 2124),
-"Arecibo":     (18.34417, -66.75278, 323),
-"LOFAR":       (52.90889, 6.86889, 6),
-"JodrellBank": (53.23625, -2.307139, 77),
-"Effelsberg":  (50.5247, 6.8828, 346),
-"MeerKAT":     (-30.721, 21.411, 1300)
-}
+site_dict = dict(MWA=(-26.7033, 116.671, 377.827),
+                 PKS=(-32.999944, 148.262306, 372),
+                 ATCA=(-30.312778, 149.550278, 209),
+                 ASKAP=(-26.696, 116.637, 372),
+                 MOST=(-35.370707, 149.424658, 737),
+                 GMRT=(19.096517, 74.049742, 656),
+                 LWA=(34.07, -107.63, 2124),
+                 GBT=(38.4331, -79.8397, 807),
+                 VLA=(34.078749, -107.618283, 2124),
+                 Arecibo=(18.34417, -66.75278, 323),
+                 LOFAR=(52.90889, 6.86889, 6),
+                 JodrellBank=(53.23625, -2.307139, 77),
+                 Effelsberg=(50.5247, 6.8828, 346),
+                 MeerKAT=(-30.721, 21.411, 1300))
 
 
 # Nominal telescope elevation limits (in degrees)
-elev_limits_dict = {
-"PKS": 30,
-"GBT": 5,
-"ATCA": 12,
-"ASKAP": 15,
-"GMRT": 17,
-"VLA": 20, # limit for high frequnecies (S-band and above), can go to 10d for L and P-band
-"Arecibo": 70,
-"Effelsberg": 8,
-"MeerKAT": 15
-}
+elev_limits_dict = dict(MWA=10, PKS=30, GBT=5,
+                        ATCA=12, ASKAP=15, GMRT=17,
+                        VLA=20, Arecibo=70, Effelsberg=8,
+                        MeerKAT=15)
 
 
 class Observatory(object):
@@ -64,29 +54,29 @@ class Observatory(object):
     def __init__(self, name, latitude=None, longitude=None, elevation=None):
         # check if the requested telescope is in the known site dictionary
         if name in site_dict.keys():
-            print "Found observatory coordinates for '{0}': {1}".format(name, site_dict[name])
+            logger.info("Found observatory coordinates for '{0}': {1}".format(name, site_dict[name]))
             self.name = name
             self.latitude = site_dict[name][0]
             self.longitude = site_dict[name][1]
             self.elevation = site_dict[name][2]
         else:
             # if not, check to make sure lat, long and elev were provided, otherwise abort
-            print "Observatory '{0}' not found in internal list".format(name)
+            logger.warning("Observatory '{0}' not found in internal list".format(name))
             if (latitude is None) or (longitude is None) or (elevation is None):
-                print "Aborting here, there was no observatory found and one or more coordinates were not provided."
+                logger.critical("Aborting here, there was no observatory found and one or more coordinates were not provided.")
                 sys.exit(1)
             else:
-                print "Using provided name and location"
-                print "\ttelescope = {0}".format(name)
-                print "\tlon, lat, elev = ({0}, {1}, {2})".format(longitude, latitude, elevation)
+                logger.info("Using provided name and location")
+                logger.debug("\ttelescope = {0}".format(name))
+                logger.debug("\tlon, lat, elev = ({0}, {1}, {2})".format(longitude, latitude, elevation))
                 self.name = name
                 self.latitude = latitude
                 self.longitude = longitude
                 self.elevation = elevation    
 
         # determine the location of the site for later use
-        self.location = EarthLocation(lat=self.latitude * u.deg, \
-                                      lon=self.longitude * u.deg, \
+        self.location = EarthLocation(lat=self.latitude * u.deg,
+                                      lon=self.longitude * u.deg,
                                       height=self.elevation * u.m)
 
         # set default values
@@ -123,7 +113,7 @@ class Observatory(object):
             self.maxtimeLocalStr = str(self.maxtimeUTC.to_datetime(timezone=tz))[:-6]
             self.utcoffsetStr = str(self.maxtimeUTC.to_datetime(timezone=tz))[-6:]
         else:
-            print "The RA, DEC or time was not provided and so we cannot calculate the altaz of the target. Aborting."
+            logger.critical("The RA, DEC or time was not provided and so we cannot calculate the altaz of the target. Aborting.")
             sys.exit(1)
 
 
@@ -134,31 +124,31 @@ class Observatory(object):
         
 
 
-def plot_ephem(ax, times, obs, plot_sun=False, site=None):
+def plot_ephem(ax, times, obs, plot_sun=False):
     """ Given an axis object, the times (x) and observed ephemeris (y), plot the source track."""
 
     # plot the ephemeris over 24 hours
-    eph = ax.plot_date(times, obs.alt, xdate=True, ls="-", lw=2, marker='', alpha=0.6, label="{0}".format(obs.name))
+    eph = ax.plot_date(times, obs.alt, xdate=True, ls="-", lw=2, marker='', alpha=0.5, label="{0}".format(obs.name))
 
     # plot the horizon line
-    ax.axhline(0, ls="--", color='k')
+    ax.axhline(0, ls="-", lw=3, color='k')
 
     # if the telescope elevation limit is known, plot it too
     if obs.elev_limit is not None:
-        ax.axhline(obs.elev_limit, ls=":", lw=3, color=eph[0].get_color(), label="{0} elev. limit".format(obs.name))
+        ax.axhline(obs.elev_limit, lw=3, color=eph[0].get_color(), label="{0} elev. limit".format(obs.name))
 
     # plot a line marking the time of maximum elevation
     ax.axvline(date2num(obs.maxtimeUTC.datetime), ls="--", lw=2, color=eph[0].get_color())
 
     # if request, plot the Sun's ephemeris for the site
     if plot_sun:
-        ax.plot_date(times[::50], obs.sun.alt, xdate=True, ls=":", lw=2, marker='', alpha=0.4, color=eph[0].get_color())
+        ax.plot_date(times[::30], obs.sun.alt, xdate=True, ls=":", lw=2, marker='', alpha=0.4, color=eph[0].get_color())
     
     return "{0}: {1} UTC{2}".format(obs.name, obs.maxtimeLocalStr, obs.utcoffsetStr)
     
 
 
-def calculate_ephem(ra, dec, date, tzoffset=0, site="MWA", show_sun=False, center=False):
+def calculate_ephem(ra, dec, date, tzoffset=0, site=["MWA"], show_sun=False, center=False):
     """Compute the ephemeris for a target on a given day at a target position."""
 
     # first, let's set up the times we want to evaluate the source position for
@@ -182,17 +172,16 @@ def calculate_ephem(ra, dec, date, tzoffset=0, site="MWA", show_sun=False, cente
     site_max = []
     coords = SkyCoord(ra, dec, unit=(u.hourangle, u.deg))
 
-    print "\nTarget: {0}\n".format(coords.to_string('hmsdms'))
+    logger.info("Target: {0}".format(coords.to_string('hmsdms')))
     for s in site:
         o = Observatory(s)
         o.compute_target_position(coords, times, tz)
-        print "\tmax. target elevation = {0}".format(o.altmax)
-        print "\ttime of max. elevation = {0} UTC{1}".format(o.maxtimeLocalStr, o.utcoffsetStr)
-        print
+        logger.info("\tmax. target elevation  = {0}".format(o.altmax))
+        logger.info("\ttime of max. elevation = {0} UTC{1}".format(o.maxtimeLocalStr, o.utcoffsetStr))
 
         if show_sun:
-            o.compute_sun_position(times[::50])
-        site_max.append(plot_ephem(ax, plttimes, o, plot_sun=show_sun, site=s))
+            o.compute_sun_position(times[::30])
+        site_max.append(plot_ephem(ax, plttimes, o, plot_sun=show_sun))
 
     # plotting aesthetics
     ax.set_xlim(min(plttimes), max(plttimes))
@@ -232,16 +221,23 @@ def calculate_ephem(ra, dec, date, tzoffset=0, site="MWA", show_sun=False, cente
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Determine ephemeris for source object over 24hrs.")
-    parser.add_argument('--ra',      type=str,   metavar="RAJ2000",  help="RAJ2000 coordinate of target source (hh:mm:ss.ss)", default=None)
-    parser.add_argument('--dec',     type=str,   metavar="DECJ2000", help="DECJ2000 coordinate of target source (dd:mm:ss.ss)", default=None)
-    parser.add_argument('--utcdate', type=str,   metavar="date",     help="Desired ephemeris UTC date (YYYY/MM/DD)", default=None)
-    parser.add_argument('--utcoff',  type=float, metavar="offset",   help="Hour offset from UTC [default = 0]", default=0)
-    parser.add_argument('--site',    type=str,   metavar="name", nargs='+', 
-                            choices=site_dict.keys(), help="Common radio telescope sites to use as observer position. "
-                            "Choose from: {0}. No default.".format(site_dict.keys()), default=None)
-    parser.add_argument('--sun', action='store_true', help="Also plot the Sun's ephemeris for each location")
+    parser.add_argument('-r', '--ra', type=str, metavar="RAJ2000",
+                        help="RAJ2000 coordinate of target source (hh:mm:ss.ss)", default=None)
+    parser.add_argument('-d', '--dec', type=str, metavar="DECJ2000",
+                        help="DECJ2000 coordinate of target source (dd:mm:ss.ss)", default=None)
+    parser.add_argument('-u', '--utcdate', type=str,   metavar="date", help="Desired ephemeris UTC date (YYYY/MM/DD)",
+                        default=None)
+    parser.add_argument('-o', '--utcoff',  type=float, metavar="offset", help="Hour offset from UTC [default = 0]",
+                        default=0)
+    parser.add_argument('-s', '--site',    type=str,   metavar="name", nargs='+', choices=site_dict.keys(),
+                        help="Common radio telescope sites to use as observer position. "
+                             "Choose from: {0} [default = MWA]".format(tuple(site_dict.keys())), default=["MWA"])
+    parser.add_argument('--sun', action='store_true', help="Plot the Sun's ephemeris for each location")
+    parser.add_argument('--draw_peak', action='store_true',
+                        help="Draw a vertical line representing the peak elevation time")
     # TODO: need to allow any number of manually defined observing positions, but for now just use those in the site_dict
-    #parser.add_argument('--observer', type=float, nargs=3, metavar=("lat", "lon", "elev"), help="Latitude (deg), longitude (deg) and elevation (m) of observer. No default.",default=(None,None,None))
+    #parser.add_argument('--observer', type=float, nargs=3, metavar=("lat", "lon", "elev"),
+    # help="Latitude (deg), longitude (deg) and elevation (m) of observer. No default.",default=(None,None,None))
     # TODO: is centering really necessary?
     #parser.add_argument('-c', '--center', action='store_true', help="Center the time of maximum elevation on the plot")
     parser.add_argument('-V', '--version', action='store_true', help="Print version and quit")
@@ -250,18 +246,27 @@ if __name__ == "__main__":
     if args.version:
         try:
             import version
-            print(version.__version__)
+            logger.debug(version.__version__)
             sys.exit(0)
         except ImportError as ie:
-            print("Couldn't import version.py - have you installed vcstools?")
-            print("ImportError: {0}".format(ie))
+            logger.error("Couldn't import version.py - have you installed vcstools?")
+            logger.error("ImportError: {0}".format(ie))
             sys.exit(0)
 
+
+    logger.setLevel(logging.DEBUG)
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(asctime)s  %(name)s  %(levelname)-9s :: %(message)s')
+    ch.setFormatter(formatter)
+    logger.addHandler(ch)
+
+
     if (args.ra is None) or (args.dec is None) or (args.utcdate is None):
-        print "ERROR: You must specify a RA, Dec and UTC date to plot the ephemeris"
+        logger.error("You must specify a RA, Dec and UTC date to plot the ephemeris")
 
     if args.site is None:
-        print "WARNING: You didn't provide a site name, assuming MWA..."
+        logger.warning("You didn't provide a site name, assuming MWA...")
         args.site = ["MWA"]
 
     calculate_ephem(args.ra, args.dec, args.utcdate, tzoffset=args.utcoff, site=args.site, show_sun=args.sun)
