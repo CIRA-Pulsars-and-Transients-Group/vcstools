e6e7a8c89752deee352ac471bc0857492a4b5f3f
keegansmith21 <keegan.r.s21@gmail.com>
2019-03-19 14:35:34 +0800

    UNTESTED. Converstion to py3. Note: check urllib2 module on galaxy

--- scripts/obs_query.py
+++ scripts/obs_query.py
@@ -3,7 +3,9 @@
 import urllib
 import urllib2
 import json
+import logging
 
+logger = logging.getLogger(__name__)
 # Append the service name to this base URL, eg 'con', 'obs', etc.
 BASEURL = 'http://mwa-metadata01.pawsey.org.au/metadata/'
 
@@ -24,16 +26,16 @@ def getmeta(service='obs', params=None):
   if service.strip().lower() in ['obs', 'find', 'con']:
     service = service.strip().lower()
   else:
-    print "invalid service name: %s" % service
+    logging.error("invalid service name: %s", service)
     return
 
   try:
     result = json.load(urllib2.urlopen(BASEURL + service + '?' + data))
   except urllib2.HTTPError as error:
-    print "HTTP error from server: code=%d, response:\n %s" % (error.code, error.read())
+    logging.error("HTTP error from server: code=%d, response:\n %s", error.code, error.read())
     return
   except urllib2.URLError as error:
-    print "URL or network error: %s" % error.reason
+    logging.error("URL or network error: %s", error.reason)
     return
 
   return result
@@ -52,11 +54,11 @@ def print_info(obs_id):
     """
 
     obsinfo = getmeta(service='obs', params={'obs_id':str(obs_id)})
-    
-    print "Obs ID:", obs_id
-    print "Name:", obsinfo['obsname']
-    print "Channels:", obsinfo['rfstreams']['0']['frequencies']
-    print "Duration:", obsinfo['stoptime'] - obsinfo['starttime'], "seconds"
+
+    logging.info("Obs ID %s:", obs_id)
+    logging.info("Name: %s", obsinfo['obsname'])
+    logging.info("Channels: %s", obsinfo['rfstreams']['0']['frequencies'])
+    logging.info("Duration: %s", obsinfo['stoptime'] - obsinfo['starttime'], "seconds")
     #times=[file[11:21] for file in obsinfo['files'] if is_number(file[11:21])] #Make a list of gps times excluding non-numbers from list
     #obs_start = int(min(times))
     #obs_end = int(max(times))
@@ -68,7 +70,3 @@ def print_info(obs_id):
 if __name__ == '__main__':
     from sys import argv
     print_info(argv[1])
-
-
-
-
