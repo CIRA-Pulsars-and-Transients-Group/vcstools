bfc0d9b212ee42bceeec627d39b0946d46b61295
NickSwainston <nickaswainston@gmail.com>
2019-05-01 12:22:36 +1000

    Upraded file_maxmin.py to python 3 and made it so it can be used as a function

--- scripts/file_maxmin.py
+++ scripts/file_maxmin.py
@@ -8,17 +8,20 @@ def print_minmax(obs_id):
     """
 
     obsinfo = getmeta(service='obs', params={'obs_id':str(obs_id)})
-    times=[file[11:21] for file in obsinfo['files'] if is_number(file[11:21])] #Make a list of gps times excluding non-numbers from list
+    times=[file[11:21] for file in obsinfo['files'] if file[11:21].isnumeric()] #Make a list of gps times excluding non-numbers from list
     obs_start = int(min(times))
     obs_end = int(max(times))
+    obs_dur = len(obsinfo['files'])
 
-    print "{0} file found with Observation ID {1}".format(len(obsinfo['files']), obs_id)
-    print "First file: {0}".format(obs_start)
-    print "Last file: {0}".format(obs_end)
+    return obs_start, obs_end, obs_dur
 
 if __name__ == '__main__':
     from sys import argv
-    print_minmax(argv[1])
+    obs_start, obs_end, obs_dur = print_minmax(argv[1])
+    print("{0} file found with Observation ID {1}".format(obs_dur, argv[1]))
+    print("First file: {0}".format(obs_start))
+    print("Last file: {0}".format(obs_end))
+    
 
 
 
