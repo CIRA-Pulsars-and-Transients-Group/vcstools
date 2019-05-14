776cae8ca940ae1f70f8201fbba26c11956198ac
keegansmith21 <keegan.r.s21@gmail.com>
2019-05-14 09:19:46 +0800

    Fixed comments. Added some error handling. OBSID is now a positional argument.

--- scripts/obs_query.py
+++ scripts/obs_query.py
@@ -13,9 +13,15 @@ def print_info(obs_id):
     """
 
     obsinfo = getmeta(service='obs', params={'obs_id':str(obs_id)})
-
+    
     logger.info("Obs ID: {0}".format(obs_id))
-    logger.info("Observation Name: {0}".format(obsinfo['obsname']))
+    #See if OBS ID is valid. If not, exit
+    try:
+        logger.info("Observation Name: {0}".format(obsinfo['obsname']))
+    except:
+        import sys
+        logger.error("Could not get obs name - is the obs id correct?: {0}".format(obs_id))
+        sys.exit(0)   
     logger.info("Channels: {0}".format(obsinfo['rfstreams']['0']['frequencies']))
     logger.info("Start Time: {0}".format(obsinfo['starttime']))
     logger.info("Stop Time: {0}".format(obsinfo['stoptime']))
@@ -28,18 +34,19 @@ if __name__ == '__main__':
     #dictionary for choosing log-levels
     loglevels = dict(DEBUG=logging.DEBUG,
                     INFO=logging.INFO,
-                    WARNING=logging.WARNING)
+                    WARNING=logging.WARNING,
+                    ERROR = logging.ERROR)
     
     #Arguments
     parser = argparse.ArgumentParser(description="""Returns information on a given OBS ID""")
-    #parser.add_argument('-o','--obsid',type=str, help='Input OBS IDs in the format " -o 1099414416"')
-    parser.add_argument('-o', '--obsid', type=int, help='Input Observation ID')
+    parser.add_argument('obsid', type=int, help='Input Observation ID in the format "-o 1099414416"')
     parser.add_argument("-L", "--loglvl", type=str, help="Logger verbosity level. Default: INFO",
                                     choices=loglevels.keys(), default="INFO")
-    parser.add_argument("-V", "--version", action="store_true", help="Print version and quit")
+    parser.add_argument("-V", "--version", action="store_true", help="Print version and quit. Currently this requires an obsid to work. Any number will suffice.")
+    #TODO: Fix the above line so that obsid is not required for the user to see the version  
+
     args = parser.parse_args()
     
-
     logger.setLevel(loglevels[args.loglvl])
     ch = logging.StreamHandler()
     ch.setLevel(loglevels[args.loglvl])
@@ -52,11 +59,12 @@ if __name__ == '__main__':
         import sys
         try:
             import version
-            print(version.__version__)
+            logger.info("Version: {0}".format(version.__version__))
             sys.exit(0)
         except ImportError as ie:
-            print("Couldn't import version.py - have you. installed vcstools?")
-            print("ImportError: {0}".format(ie))
+            logger.error("Couldn't import version.py - have you. installed vcstools?")
+            logger.error(("ImportError: {0}".format(ie)))
+
             sys.exit(0)
 
 
