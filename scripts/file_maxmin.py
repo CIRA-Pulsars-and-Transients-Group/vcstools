f26948e431051bf98726954e10d778fa3a7cf521
NickSwainston <nickaswainston@gmail.com>
2019-05-19 19:44:02 +1000

    Made all headers #!/usr/bin/python3

--- scripts/file_maxmin.py
+++ scripts/file_maxmin.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 from mwa_metadb_utils import getmeta, is_number
 
--- utils/config.py
+++ utils/config.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 """
 Functions to handle parsing the config file for multiple super computers
