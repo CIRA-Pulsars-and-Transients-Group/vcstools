ecef6bbe3af14ece27124ed01eb69c32511a145e
keegansmith21 <keegan.r.s21@gmail.com>
2019-06-11 10:49:42 +0800

    Added logging and argparse. Currently not fucntional

--- scripts/recombine.py
+++ scripts/recombine.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
 import subprocess
 import os
@@ -6,20 +6,55 @@ import sys
 import glob
 import getopt
 from mpi4py import MPI
+import logging
+import argparse
+
+logger = logging.getLogger(__name__)
+
+
 
 def usage(opts={}):
-    print "recombine.py -s <start time> -o <obsid> -w <working directory> -e <recombine executable>\n"
+    logger.info("recombine.py -s <start time> -o <obsid> -w <working directory> -e <recombine executable>\n")
 
 testsize = 327680000
 
 
 if __name__ == '__main__':
 
+    from sys import argv
 
     the_options = {'recombine': "recombine", 'start': int(0), 'root' : "./", 'obsid' : int(0), 'testit' : 0, 'skip' : " ", 'read_pfb' : 1}
+  
+    loglevels = dict(DEBUG=logging.DEBUG,
+                    INFO=logging.INFO,
+                    WARNING=logging.WARNING,
+                    ERROR = logging.ERROR)    
+ 
     
+    #Arguments
+    parser = argparse.ArgumentParser(description="""Combines raw VCS data""")
+    parser.add_argument("-o", "--obsid", type=int, help="Input Observation ID")
+    parser.add_argument("-s", "--start", type=int, help="GPS time to test")
+    parser.add_argument("-w", "--data_dir", type=str, help="Directory containing the raw data")
+    parser.add_argument("-e", "--recombine", type=str, help="filename of the recombine function") 
+
+    args = parser.parse_args()
+    
+    #logger.setLevel(loglevels[args.loglvl])
+    #logger.setLevel(DEBUG)
+    #ch = logging.StreamHandler()
+    #ch.setLevel(loglevels[args.loglvl])
+    #ch.setLevel(DEBUG)
+    #formatter = logging.Formatter('%(asctime)s  %(filename)s  %(name)s  %(lineno)-4d  %(levelname)-9s :: %(message)s')
+    #ch.setFormatter(formatter)
+    #logger.addHandler(ch)
+
+
+    """
     try:
         opts, args = getopt.getopt(sys.argv[1:],"hce:o:s:tw:")
+        logger.debug("options: {0}".format(opts))
+        logger.debug("arguments: {0}".format(args))
     except getopt.GetoptError:
         usage(the_options)
         sys.exit(-1)
@@ -45,6 +80,15 @@ if __name__ == '__main__':
         elif (opt == "-c"):
             the_options['skip'] = "-c"
             the_options['read_pfb'] = 0
+    """
+    #Setting the_options to the arg inputs
+    the_options["obsid"] = args.obsid
+    the_options["start"] = args.start
+    the_options["root"] = args.data_dir
+    the_options["recombine"] = args.recombine
+    
+    logger.info("Input Options:")
+    usage(the_options)
 
     if (the_options['start'] == 0):
         usage(the_options)
@@ -57,7 +101,10 @@ if __name__ == '__main__':
     rank = comm.Get_rank()
 
     time_to_combine = the_options['start']+rank
-    files_glob = "%s/combined/%d_%d_ch*.dat" % (the_options['root'],the_options['obsid'],time_to_combine)
+    #files_glob = "{0}/combined/{1}_{2}_ch*.dat" % (the_options['root'],the_options['obsid'],time_to_combine)
+    #Need to change the 
+    files_glob = "{0}/recombine_test_delete_this/{1}_{2}_ch*.dat".format(the_options['root'], the_options['obsid'], time_to_combine)
+ 
     broken = 24;
     for to_check in sorted(glob.glob(files_glob)):
         file_statinfo = os.stat(to_check)
@@ -70,18 +117,18 @@ if __name__ == '__main__':
         f=[]
         for vcs in range(1,17):
             for stream in [0,1]:
-                file_to_combine = "%d_%d_vcs%02d_%1d.dat " % (the_options['obsid'],time_to_combine,vcs,stream)
+                file_to_combine = "{0}_{1}_vcs{2}02_{3}1.dat ".format(the_options['obsid'],time_to_combine,vcs,stream)
                 f.append(file_to_combine)
 
 
-        recombine_line = "%s %s -o %s -t %d -m %s/%d_metafits_ppds.fits -i %s/combined -f " % (the_options['recombine'],the_options['skip'],the_options['obsid'],time_to_combine,the_options['root'],the_options['obsid'],the_options['root'])
+        recombine_line = "{0} {1} -o {2} -t {3} -m {4}/{5}_metafits_ppds.fits -i {6}/recombine_test_delete_this -f ".format(the_options['recombine'],the_options['skip'],the_options['obsid'],time_to_combine,the_options['root'],the_options['obsid'],the_options['root'])
 
 
 
         for f_to_r in f:
             recombine_line = "{0} {1}/raw/{2}".format(recombine_line, the_options['root'],f_to_r)
 
-        recombine_line = "%s\n" % recombine_line
+        recombine_line = "{0}\n".format(recombine_line)
         #print "this is what is called: " + recombine_line
         #subprocess.call("which recombine",shell=True)
         subprocess.call(recombine_line,shell=True)
