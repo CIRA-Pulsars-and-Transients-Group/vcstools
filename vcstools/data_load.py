5ff1d1e7af0823522e23cdf84a44a3dd2b7d69b4
keegansmith21 <keegan.r.s21@gmail.com>
2020-07-15 13:28:58 +0800

    Added the base directory to this

--- vcstools/data_load.py
+++ vcstools/data_load.py
@@ -3,6 +3,7 @@ Loads all the data required by vcstools from the data directory.
 """
 
 import os
+from config_vcs import load_config_file
 
 datadir = os.path.join(os.path.dirname(__file__), 'data')
 
@@ -12,6 +13,9 @@ TRCVR_FILE = os.path.join(datadir, 'MWA_Trcvr_tile_56.csv')
 # Hard code the paths of pulsar search files
 KNOWN_RFRB_CSV = os.path.join(datadir, 'known_repeating_FRBs.csv')
 POI_CSV = os.path.join(datadir, 'points_of_interest.csv')
+
+# hard code the base product directory
+BASE_DIR = load_config_file()['base_product_dir']
 
 # Hard code the path of the ATNF psrcat database file
 ATNF_LOC = os.path.join(datadir, 'psrcat.db')
