47649df6855a17b7f84e1920ce08f0ea0c2141ea
Sam McSweeney <sammy.mcsweeney@gmail.com>
2020-10-13 18:55:08 +0800

    Finished making a working version of sky_model_jones_comparison.c. Compiled and tested on my local machine.

--- tests/sky_model_jones_comparison.c
+++ tests/sky_model_jones_comparison.c
@@ -20,6 +20,9 @@
 #define MWA_LAT -26.703319             /* Array latitude. degrees North */
 #define DD2R      1.74532925199433e-02 /* = PI/180 */
 #define DPIBY2    1.570796326794897    /* = PI/2   */
+#define NDELAYS  16
+
+void sky_model_parse_cmdline( int , char **, struct make_beam_opts * );
 
 int main(int argc, char **argv)
 {
@@ -34,17 +37,10 @@ int main(int argc, char **argv)
     opts.frequency   = 0;    // = rec_channel expressed in Hz
 
     // Variables for MWA/VCS configuration
-    opts.nstation      = 128;    // The number of antennas
-    opts.nchan         = 128;    // The number of fine channels (per coarse channel)
     opts.chan_width    = 10000;  // The bandwidth of an individual fine chanel (Hz)
-    opts.sample_rate   = 10000;  // The VCS sample rate (Hz)
 
     // Parse command line arguments
-    make_beam_parse_cmdline( argc, argv, &opts );
-
-    // Create "shorthand" variables for options that are used frequently
-    int nstation             = opts.nstation;
-    int nchan                = opts.nchan;
+    sky_model_parse_cmdline( argc, argv, &opts );
 
     // Read in info from metafits file
     struct metafits_info mi;
@@ -52,11 +48,11 @@ int main(int argc, char **argv)
 
     // Always use the first tile/pol's set of delays and assume no dead
     // dipoles
-    unsigned int *delays = (unsigned int *)mi->delays[0];
-    double *amps = { 0.0, 0.0, 0.0, 0.0,
-                     0.0, 0.0, 0.0, 0.0,
-                     0.0, 0.0, 0.0, 0.0,
-                     0.0, 0.0, 0.0, 0.0 };
+    unsigned int *delays = (unsigned int *)mi.delays[0];
+    double amps[] = { 1.0, 1.0, 1.0, 1.0,
+                      1.0, 1.0, 1.0, 1.0,
+                      1.0, 1.0, 1.0, 1.0,
+                      1.0, 1.0, 1.0, 1.0 };
 
     // Load the FEE2016 beam model
     FEEBeam *beam = new_fee_beam( HYPERBEAM_HDF5 );
@@ -76,12 +72,15 @@ int main(int argc, char **argv)
     double         *JF;    // Beam model Jones -- FEE2016
 
     double az, za; // (az)imuth & (z)enith (a)ngle in radians
+    int zenith_norm = 1; // Normalise FEE2016 beam to zenith
 
     int i; // Generic loop counter
 
     while (fscanf( fP, "%lf %lf", &az, &za ) != EOF)
     {
         // Calculate jones matrix for the ANALYTIC beam
+        //fprintf( stderr, "calcEjones_analytic( JA, %ld, %lf, %lf, %lf, %lf, %lf )\n",
+        //        opts.frequency, (MWA_LAT*DD2R), mi.tile_pointing_az*DD2R, (DPIBY2-(mi.tile_pointing_el*DD2R)), az, za );
         calcEjones_analytic( JA,                      // pointer to 4-element (2x2) voltage gain Jones matrix
                 opts.frequency,                       // observing freq of fine channel (Hz)
                 (MWA_LAT*DD2R),                       // observing latitude (radians)
@@ -90,6 +89,8 @@ int main(int argc, char **argv)
                 az, za );                             // azimuth & zenith angle of pencil beam
 
         // Calculate jones matrix for the ANALYTIC beam
+        //fprintf( stderr, "calc_jones( beam, %lf, %lf, %ld, delays, amps, %d\n",
+        //        az, za, opts.frequency, zenith_norm );
         JF = calc_jones( beam, az, za, opts.frequency, delays, amps, zenith_norm );
 
         // Write out the matrices to the respective files
@@ -111,14 +112,22 @@ int main(int argc, char **argv)
         free( JF );
     }
 
+    // Uncomment the below for verbose debugging information
+    /*
+    fprintf( stderr, "delays = [ " );
+    for (i = 0; i < NDELAYS; i++)
+        fprintf( stderr, "%u ", delays[i] );
+    fprintf( stderr, "]\namps = [ " );
+    for (i = 0; i < NDELAYS; i++)
+        fprintf( stderr, "%.1f ", amps[i] );
+    fprintf( stderr, "]\n" );
+    */
+
     // Close files for writing
     fclose(fF);
     fclose(fA);
 
     // Free up memory
-    destroy_complex_weights( complex_weights_array, npointing, nstation, nchan );
-    destroy_invJi( invJi, nstation, nchan, npol );
-
     destroy_metafits_info( &mi );
 
     free( opts.pointings    );
@@ -136,30 +145,26 @@ void usage() {
     fprintf(stderr, "usage: sky_model_jones_comparison [OPTIONS]\n");
 
     fprintf(stderr, "\n");
+    fprintf(stderr, "DESCRIPTION\n");
+    fprintf(stderr, "\n");
+    fprintf(stderr, "\tCalculates the Jones matrices for both the original "
+                      "analytic beam and the FEE2016 beam for a tile\n"
+                    "\tpointing given in the supplied metafits file and a "
+                      "list of tied-array beam pointings supplied in\n"
+                    "\tthe supplied two-column ASCII file. Two files are "
+                      "generated: sky_model_comparison_ANALYTIC.dat\n"
+                    "\tand sky_model_comparison_FEE2016.dat.\n" );
+
+    fprintf(stderr, "\n");
     fprintf(stderr, "REQUIRED OPTIONS\n");
     fprintf(stderr, "\n");
     fprintf(stderr, "\t-p, --pointing_file=FILE  ");
-    fprintf(stderr, "Two-column ascii file containing az and za (in deg)\n");
-    fprintf(stderr, "\n");
+    fprintf(stderr, "Two-column ascii file containing az and za (in rad)\n");
     fprintf(stderr, "\t-m, --metafits-file=FILE  ");
     fprintf(stderr, "FILE is the metafits file\n");
-    fprintf(stderr, "\n");
     fprintf(stderr, "\t-f, --frequency=FREQ      ");
     fprintf(stderr, "FREQ is the frequency in Hz");
     fprintf(stderr, "\n");
-
-    fprintf(stderr, "\n");
-    fprintf(stderr, "MWA/VCS CONFIGURATION OPTIONS\n");
-    fprintf(stderr, "\n");
-    fprintf(stderr, "\t-a, --antennas=N          ");
-    fprintf(stderr, "The number of antennas in the array. For MWA Phase 2, N=128.     ");
-    fprintf(stderr, "[default: 128]\n");
-    fprintf(stderr, "\t-n, --num-fine-chans=N    ");
-    fprintf(stderr, "The number of fine channels per coarse channel.                  ");
-    fprintf(stderr, "[default: 128]\n");
-    fprintf(stderr, "\t-w, --fine-chan-width=N   ");
-    fprintf(stderr, "The bandwidth of an individual fine channel (Hz).                ");
-    fprintf(stderr, "[default: 10000]\n");
 
     fprintf(stderr, "\n");
     fprintf(stderr, "OTHER OPTIONS\n");
@@ -173,7 +178,7 @@ void usage() {
 
 
 
-void make_beam_parse_cmdline(
+void sky_model_parse_cmdline(
         int argc, char **argv, struct make_beam_opts *opts )
 {
     if (argc > 1) {
@@ -182,28 +187,22 @@ void make_beam_parse_cmdline(
         while (1) {
 
             static struct option long_options[] = {
-                {"antennas",        required_argument, 0, 'a'},
                 {"frequency",       required_argument, 0, 'f'},
                 {"help",            required_argument, 0, 'h'},
                 {"metafits-file",   required_argument, 0, 'm'},
-                {"num-fine-chans",  required_argument, 0, 'n'},
                 {"pointings_file",  required_argument, 0, 'p'},
-                {"version",         required_argument, 0, 'V'},
-                {"fine-chan-width", required_argument, 0, 'w'}
+                {"version",         required_argument, 0, 'V'}
             };
 
             int option_index = 0;
             c = getopt_long( argc, argv,
-                             "a:f:hm:n:p:r:Vw:",
+                             "f:hm:p:V",
                              long_options, &option_index);
             if (c == -1)
                 break;
 
             switch(c) {
 
-                case 'a':
-                    opts->nstation = atoi(optarg);
-                    break;
                 case 'f':
                     opts->frequency = atoi(optarg);
                     break;
@@ -214,9 +213,6 @@ void make_beam_parse_cmdline(
                 case 'm':
                     opts->metafits = strdup(optarg);
                     break;
-                case 'n':
-                    opts->nchan = atoi(optarg);
-                    break;
                 case 'p':
                     opts->pointings = strdup(optarg);
                     break;
@@ -224,11 +220,8 @@ void make_beam_parse_cmdline(
                     fprintf( stderr, "MWA Beamformer %s\n", VERSION_BEAMFORMER);
                     exit(0);
                     break;
-                case 'w':
-                    opts->chan_width = atoi(optarg);
-                    break;
                 default:
-                    fprintf(stderr, "error: make_beam_parse_cmdline: "
+                    fprintf(stderr, "error: sky_model_parse_cmdline: "
                                     "unrecognised option '%s'\n", optarg);
                     usage();
                     exit(EXIT_FAILURE);
