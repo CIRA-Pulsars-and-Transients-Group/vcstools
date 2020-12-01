21589ff1ed0a1e4159642c9a4291dcd95cd27e33
NickSwainston <nickaswainston@gmail.com>
2020-12-01 16:23:50 +0800

    Added metadb_util tests

--- tests/test_mwa_metadb_utils.py
+++ tests/test_mwa_metadb_utils.py
@@ -5,6 +5,18 @@ Tests the mwa_metadb_utils.py script
 from vcstools.metadb_utils import mwa_alt_az_za, getmeta, get_obs_array_phase
 from numpy.testing import assert_almost_equal
 
+# Can't test singles_source_search or find_obsids_meta_pages because it's time consuming and always updating
+
+def test_get_obs_array_phase():
+    """Test FWHM calculation"""
+    for obsid, expect_ans in [(1117101752, 'P1'),
+                              (1252177744, 'P2C'),
+                              (1247832024, 'P2E'),
+                              (1188439520, 'OTH')]:
+        ans = get_obs_array_phase(obsid)
+        if ans != expect_ans:
+            raise AssertionError()
+
 
 def test_mwa_alt_az_za():
     """Test the mwa_alt_az_za function"""
@@ -20,16 +32,48 @@ def test_mwa_alt_az_za():
         assert_almost_equal(az,  exp_az,  decimal=3)
         assert_almost_equal(za,  exp_za,  decimal=3)
 
-def test_get_obs_array_phase():
-    """Test FWHM calculation"""
-    for obsid, expect_ans in [(1117101752, 'P1'),
-                              (1252177744, 'P2C'),
-                              (1247832024, 'P2E'),
-                              (1188439520, 'OTH')]:
-        ans = get_obs_array_phase(obsid)
-        if ans != expect_ans:
-            raise AssertionError()
+def test_get_common_obs_metadata():
+    """Test the get_common_obs_metadata function"""
 
+    expect = [1117101752, 141.662872328864, 10.6540169083522, 4800,
+              [[21, 19, 17, 15, 16, 14, 12, 10, 11, 9, 7, 5, 6, 4, 2, 0],
+               [21, 19, 17, 15, 16, 14, 12, 10, 11, 9, 7, 5, 6, 4, 2, 0]],
+              184.96, [133, 134, 135, 136, 137, 138, 139, 140, 141, 142, 143, 144,
+                       145, 146, 147, 148, 149, 150, 151, 152, 153, 154, 155, 156]]
+    ans = get_common_obs_metadata(1117101752)
+        if ans != expect:
+            raise AssertionError
+def test_get_files():
+    """Test the get_files function"""
+
+    if get_files(1266823286) != ['1266823286_metafits_ppds.fits']:
+        raise AssertionError
+
+def test_calc_ta_fwhm():
+    """Test the calc_ta_fwhm function"""
+
+    tests = [(1133775752, 4.208367616629838e-08),
+             (1164110416, 4.098704979920858e-08),
+             (1194350120, 3.9949300287565104e-08)]
+
+    for obsid, expect_fwhm in tests:
+        ans = calc_ta_fwhm(obsid)
+        assert_almost_equal(ans, expect_fwhm, decimal=10)
+
+def test_get_channels():
+    """Test the get_channels function"""
+
+    if get_channels(1133775752) != [133, 134, 135, 136, 137, 138,
+                                    139, 140, 141, 142, 143, 144,
+                                    145, 146, 147, 148, 149, 150,
+                                    151, 152, 153, 154, 155, 156]:
+        raise AssertionError
+
+def test_obs_max_min():
+    """Test the obs_max_min function"""
+
+    if obs_max_min(1133775752) != (1133775759, 1133780672):
+        raise AssertionError
 
 if __name__ == "__main__":
     # introspect and run all the functions starting with 'test'
