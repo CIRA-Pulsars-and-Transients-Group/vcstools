021b0e7060de4cf85821167553f0d0d957c6fa02
NickSwainston <nickaswainston@gmail.com>
2020-12-01 14:23:01 +0800

    Updated the process_vcs test to general_utils tests

--- tests/test_general_utils.py
+++ tests/test_general_utils.py
@@ -2,13 +2,13 @@
 """
 Tests the find_pulsar_in_obs.py script
 """
-import process_vcs
+from vcstools.general_utils import gps_to_utc
 
 def test_gps_to_utc():
     """test the gps_to_utc function"""
     tests = [(1111111111, '2015-03-23T01:58:15'), (1222222222, '2018-09-29T02:10:04')]
     for obs, exp_utc in tests:
-        utc = process_vcs.gps_to_utc(obs)
+        utc = gps_to_utc(obs)
         if utc != exp_utc:
             raise AssertionError
 
