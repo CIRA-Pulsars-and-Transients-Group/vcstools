d688e63504b47ab96c9dab8e38827f83cae81e73
NickSwainston <nickaswainston@gmail.com>
2021-01-25 18:01:15 +0800

    Increased the mwa beam calc test to a largers size and added benchmarking (may reduce this again later)

--- tests/test_mwa_hyperbeam.py
+++ tests/test_mwa_hyperbeam.py
@@ -2,6 +2,7 @@
 """Compares the results of the FEE beam model using mwa_pb and mwa_hyperbeam."""
 import numpy as np
 from numpy.testing import assert_almost_equal
+import time
 
 from mwa_pb import primary_beam
 from mwa_pb import config
@@ -12,7 +13,7 @@ def test_hyperbeam_vs_pb():
     beam = mwa_hyperbeam.FEEBeam(config.h5file)
 
     # Set up fake data.
-    n = 1000
+    n = 1000000
     az = np.linspace(0, 0.9 * np.pi, n)
     za = np.linspace(0.1, 0.9 * np.pi / 2, n)
     freq = 167000000
@@ -22,15 +23,20 @@ def test_hyperbeam_vs_pb():
 
     # Jones --------------------------------------------------
     # mwa_pb method
+    start_time = time.perf_counter()
     pb_jones = primary_beam.MWA_Tile_full_EE(za, az,
-                                             freq=freq, delays=delays,
-                                             zenithnorm=True,
-                                             power=True,
-                                             jones=True)
-      
+                                                freq=freq, delays=np.array(delays),
+                                                zenithnorm=True,
+                                                power=True,
+                                                jones=True,
+                                                interp=False)
+    print("mwa_pb benchmark: {} s".format(time.perf_counter()-start_time))
+
     # hyperbeam method
-    print(freq, delays, amps)
+    #print(freq, delays, amps)
+    start_time = time.perf_counter()
     hb_jones = beam.calc_jones_array(az, za, freq, delays, amps, True)
+    print("hyperbeam benchmark: {} s".format(time.perf_counter()-start_time))
     hb_jones = hb_jones.reshape(n, 2, 2)
 
     # Compare Jones
@@ -39,7 +45,7 @@ def test_hyperbeam_vs_pb():
     # Power ---------------------------------------------------
     # mwa_pb method
     pb_xx, pb_yy = primary_beam.MWA_Tile_full_EE(za, az,
-                                               freq=freq, delays=delays,
+                                               freq=freq, delays=np.array(delays),
                                                zenithnorm=True,
                                                power=True)
 
