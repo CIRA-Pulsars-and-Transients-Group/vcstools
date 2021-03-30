95cc8c845219e4e85a22b2c89f6ab7f64675f327
keegansmith21 <keegan.r.s21@gmail.com>
2021-03-30 13:37:18 +0800

    Updated the prof estimator to use the new gfit class. Also this wasn't on setup.py so i added it

--- scripts/prof_estimate.py
+++ scripts/prof_estimate.py
@@ -5,7 +5,8 @@ import os
 import logging
 import argparse
 
-from vcstools.prof_utils import get_from_bestprof, get_from_ascii, auto_gfit, prof_eval_gfit, subprocess_pdv
+from vcstools.prof_utils import get_from_bestprof, get_from_ascii, subprocess_pdv
+from vcstools.gfit import gfit
 
 logger = logging.getLogger(__name__)
 
@@ -19,21 +20,19 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="""A utility file for calculating a variety of pulse profile properties""",\
                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    inputs = parser.add_argument_group("Inputs")
-    inputs.add_argument("--bestprof", type=str, help="The pathname of the file containing the pulse profile. Use if in .pfd.bestprof format")
-    inputs.add_argument("--ascii", type=str, help="The pathname of the file containing the pulse profile. Use if in ascii text format")
-    inputs.add_argument("--archive", type=str, help="The pathname of the file containing the pulse profile. Use if in archive format")
-    inputs.add_argument("--ignore_threshold", type=float, default=0.02, help="Maxima with values below this fraction of the profile maximum will be ignored.")
-    inputs.add_argument("--min_comp_len", type=int, default=None,\
-                        help="Minimum length of a component to be considered real. Measured in bins. If none, will use 1 percent of total profile length")
-    inputs.add_argument("--alpha", type=float, default=2, help="Used by the clipping function to determine the noise level. A lower value indicates\
-                        a higher verbosity level in the noise clipping function.")
-    inputs.add_argument("--auto", action="store_true", help="Used to automatically find the best alpha value to clip this profile")
-    inputs.add_argument("--period", type=float, help="The period of the puslar in ms. Found automatically if bestprof supplied.\
-                        Used in S/N calculation. Not required")
+    file_parse = parser.add_argument_group("File Input", description="Choose only one option")
+    file_parse.add_argument("--bestprof", type=str, help="The pathname of the file containing the pulse profile. Use if in .pfd.bestprof format")
+    file_parse.add_argument("--ascii", type=str, help="The pathname of the file containing the pulse profile. Use if in ascii text format")
+    file_parse.add_argument("--archive", type=str, help="The pathname of the file containing the pulse profile. Use if in archive format")
 
     g_inputs = parser.add_argument_group("Gaussian Inputs")
     g_inputs.add_argument("--max_N", type=int, default=6, help="The maximum number of gaussian components to attempt to fit")
+    g_inputs.add_argument("--min_comp_len", type=int, default=None,\
+                        help="Minimum length of a component to be considered real. Measured in bins. If none, will use 1 percent of total profile length")
+    g_inputs.add_argument("--period", type=float, help="The period of the puslar in ms. Found automatically if bestprof supplied.\
+                        Used in S/N calculation. Not required")
+    g_inputs.add_argument("--cliptype", type=str, default="regular", choices=["regular", "noisy", "verbose"], help="The verbosity of clipping used\
+                        by the Gaussian fitter.")
 
     other_inputs = parser.add_argument_group("Other Inputs")
     other_inputs.add_argument("--plot_name", type=str, help="The name of the output plot file. If none, will not plot anything")
@@ -43,9 +42,12 @@ if __name__ == '__main__':
     logger.setLevel(loglevels[args.loglvl])
     ch = logging.StreamHandler()
     ch.setLevel(loglevels[args.loglvl])
-    formatter = logging.Formatter('%(asctime)s  %(filename)s  %(name)s  %(lineno)-4d  %(levelname)-9s :: %(message)s')
+    log_format = '%(asctime)s  %(filename)s  %(name)s  %(lineno)-4d  %(levelname)-9s :: %(message)s'
+    formatter = logging.Formatter(log_format)
     ch.setFormatter(formatter)
+    logging.basicConfig(level=loglevels[args.loglvl], format=log_format, handlers=[ch])
     logger.addHandler(ch)
+    logger.propagate=True
 
     if args.bestprof:
         _, _, _, period, _, _, _, profile, _  = get_from_bestprof(args.bestprof)
@@ -61,9 +63,8 @@ if __name__ == '__main__':
         logger.error("Please supply either an ascii or bestprof profile")
         sys.exit(1)
 
-    if args.auto:
-        auto_gfit(profile, max_N=args.max_N, ignore_threshold=args.ignore_threshold,\
-                        plot_name=args.plot_name, min_comp_len=args.min_comp_len, period=period)
-    else :
-        prof_eval_gfit(profile, max_N=args.max_N, ignore_threshold=args.ignore_threshold,\
-                        plot_name=args.plot_name, min_comp_len=args.min_comp_len, alpha=args.alpha, period=period)
+    g_fitter = gfit(profile, max_N=args.max_N, min_comp_len=args.min_comp_len, period=period,
+                    plot_name=args.plot_name, cliptype=args.cliptype)
+    g_fitter.auto_gfit()
+    if args.plot_name:
+        g_fitter.plot_fit()
