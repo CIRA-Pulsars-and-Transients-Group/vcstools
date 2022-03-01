f4282b8e60ac39325e3960afbfb1327093b0ea02
NickSwainston <nickaswainston@gmail.com>
2022-03-01 21:20:02 +0800

    Cleaned up recombine.py and added the option for different input and output directories

--- scripts/recombine.py
+++ scripts/recombine.py
@@ -12,30 +12,20 @@ from vcstools.general_utils import setup_logger
 
 logger = logging.getLogger(__name__)
 
-
-def usage(opts={}):
-    logger.info("recombine.py -s <start time> -o <obsid> -w <working directory> -e <recombine executable>\n")
-
-testsize = 327680000
-
-
 if __name__ == '__main__':
-
-
-    the_options = {'recombine': "recombine", 'start': int(0), 'root' : "./", 'obsid' : int(0), 'testit' : 0, 'skip' : " ", 'read_pfb' : 1}
-
     loglevels = dict(DEBUG=logging.DEBUG,
                     INFO=logging.INFO,
                     WARNING=logging.WARNING,
                     ERROR = logging.ERROR)
 
-
     #Arguments
     parser = argparse.ArgumentParser(description="""Combines raw VCS data""")
-    parser.add_argument("-o", "--obsid", type=int, help="Input Observation ID")
-    parser.add_argument("-s", "--start", type=int, help="GPS time to test")
+    parser.add_argument("-o", "--obsid", type=int, help="Input Observation ID.", required=True)
+    parser.add_argument("-s", "--start", type=int, help="First GPS second of data to recombine.", required=True)
+    parser.add_argument("-d", "--duration", type=int, help="Seconds of data to recomine", required=True)
     parser.add_argument("-w", "--data_dir", type=str, help="Directory containing the raw data")
-    parser.add_argument("-e", "--recombine", type=str, help="filename of the recombine function")
+    parser.add_argument("-p", "--output_base_dir", type=str, help="The base directory to put the recombined data. Eg /astro/mwavcs/vcs/<obsid>.")
+    parser.add_argument("-e", "--recombine_command", type=str, help="the filename of the recombine function. Default: recombine", default="recombine")
     parser.add_argument("-L", "--loglvl", type=str, choices=loglevels.keys(), default="INFO", help="Logger verbosity level. Default: INFO")
     parser.add_argument("-V", "--version", action="store_true", help="Print version and quit")
     args = parser.parse_args()
@@ -53,65 +43,49 @@ if __name__ == '__main__':
             logger.error("ImportError: {0}".format(ie))
             sys.exit(0)
 
-    #Setting the_options to the arg inputs
-    the_options["obsid"] = args.obsid
-    the_options["start"] = args.start
-    the_options["root"] = args.data_dir
-    the_options["recombine"] = args.recombine
-
-
-    if (the_options['start'] == 0):
-        usage(the_options)
-        sys.exit(-1)
-    if (the_options['obsid'] == 0):
-        usage(the_options)
-        sys.exit(-1)
-
     comm = MPI.COMM_WORLD
     rank = comm.Get_rank()
+    size = comm.Get_size()
 
-    time_to_combine = the_options['start']+rank
-    files_glob = "{0}/combined/{1}_{2}_ch*.dat".format(the_options['root'], the_options['obsid'], time_to_combine)
+    if size != args.duration:
+        logger.error("Wrong number of CPU cores (cores != --duration). Exiting.")
+        sys.exit(1)
 
+    # Core dependent time to recombine
+    time_to_combine = args.start + rank
 
-    broken = 24;
-    for to_check in sorted(glob.glob(files_glob)):
+    # Check if files are already succesfully combined
+    combined_files_glob = f"{args.data_dir}/combined/{args.obsid}_{time_to_combine}_ch*.dat"
+    # Check if
+    left_to_combined = 24
+    for to_check in sorted(glob.glob(combined_files_glob)):
         file_statinfo = os.stat(to_check)
 
-
-        if (file_statinfo.st_size == testsize):
-            broken = broken-1
+        if (file_statinfo.st_size == 327680000):
+            # File with correct size
+            left_to_combined -= 1
         else:
+            # File with incorrect size so delete it
             os.remove(to_check)
 
-    logger.debug("Thread {0} :: Final broken check: {1}".format(rank, broken))
+    logger.debug(f"Thread {rank} :: Final left to combined check: {left_to_combined}")
 
-    if (broken > 0):
-        logger.info("Thread {0} :: Combining raw files".format(rank))
+    if (left_to_combined > 0):
+        logger.info(f"Thread {rank} :: Combining raw files")
 
-        f=[]
+        # Create file names based on vcs box names
+        files_to_combine = []
         for vcs in range(1,17):
             for stream in [0,1]:
-                file_to_combine = "{0}_{1}_vcs{2:0=2d}_{3}.dat ".format(the_options['obsid'],time_to_combine,vcs,stream)
-                f.append(file_to_combine)
+                files_to_combine.append(f"{args.obsid}_{time_to_combine}_vcs{vcs:0=2d}_{stream}.dat ")
 
+        # Create the recombine commad
+        recombine_line = f"{args.recombine_command} -o {args.obsid} -t {time_to_combine} -m {args.output_base_dir}/{args.obsid}_metafits_ppds.fits -i {args.output_base_dir}/combined -f "
+        for raw_file in files_to_combine:
+            # Add each file
+            recombine_line += f"{args.data_dir}/{raw_file}"
 
-        recombine_line = "{0} {1} -o {2} -t {3} -m {4}/{5}_metafits_ppds.fits -i {6}/combined -f ".format(the_options['recombine'],the_options['skip'],the_options['obsid'],time_to_combine,the_options['root'],the_options['obsid'],the_options['root'])
-
-
-
-        for f_to_r in f:
-            recombine_line = "{0} {1}/raw/{2}".format(recombine_line, the_options['root'],f_to_r)
-
-        recombine_line = "{0}\n".format(recombine_line)
-        #print "this is what is called: " + recombine_line
-        #subprocess.call("which recombine",shell=True)
-        subprocess.call(recombine_line,shell=True)
-
-        #log_name="{0}/recombine_{1}.log".format(the_options['root'],time_to_combine)
-
-        #with open(log_name, 'w') as log:
-        #    subprocess.call(recombine_line,shell=True,stdout=log,stderr=log)
+        subprocess.call(recombine_line, shell=True)
     else:
         logger.warn("Combined files already present. Exiting.")
     comm.Barrier()
