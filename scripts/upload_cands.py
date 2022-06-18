36bbbf241cc538149531879360b7ffe0cb067066
Sam McSweeney <sammy.mcsweeney@gmail.com>
2022-06-18 14:33:21 +0800

    Added flexibility for uploading individual candidates

--- scripts/upload_cands.py
+++ scripts/upload_cands.py
@@ -15,7 +15,9 @@ from vcstools.pointing_utils import sex2deg
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="""Upload the candidates on prometheus""")
     parser.add_argument('-o', '--obsid', type=int,
-            help='The observation ID of the MWA observation')
+            help="The observation ID of the MWA observation")
+    parser.add_argument('-a', '--all', action='store_true',
+            help="Load all PFD files in the current directory (overrides -p)")
     parser.add_argument('-b', '--begin', type=int,
             help="Begin time gps")
     parser.add_argument('-e', '--end', type=int,
@@ -28,8 +30,11 @@ if __name__ == "__main__":
             help="Maximum number of threads to use for concurrent submission.")
     parser.add_argument('-s', '--super_computer_id', type=int, default=1,
             help="Super computer ID. Ozstar: 1. Garrawarla: 2. SHAO: 3. Galaxy: 4. Magnus: 5. Default 1.")
+    parser.add_argument('-p', '--pfd_file', type=str,
+            help="The PFD file to be uploaded. This can be overridden by -a")
     args = parser.parse_args()
 
+    '''
     print("Uploading obsid")
     try:
         upload_obsid(args.obsid)
@@ -45,9 +50,18 @@ if __name__ == "__main__":
                     mwa_search_command=args.command, mwa_search_version='v3.0', supercomputer_id=args.super_computer_id)
     else:
         print("WARNING: Not uploading beams")
+        pointing_list = None
+    '''
+
+    if args.pfd_file is None and args.all == False:
+        raise RuntimeError("Either -p or -a must be supplied")
 
     print("Uploading cands")
-    pfd_files = glob.glob('*pfd')
+    if args.all:
+        pfd_files = glob.glob('*pfd')
+    else:
+        pfd_files = [args.pfd_file]
+
     upload_cand(pfd_files, obsid=args.obsid,
                 search_type='SMART_10min', search_params_id=1)
 
