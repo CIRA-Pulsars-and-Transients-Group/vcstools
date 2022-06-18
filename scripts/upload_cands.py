482af8e3d3c4632e7a742a017ed74f7d7b8278e8
Sam McSweeney <sammy.mcsweeney@gmail.com>
2022-06-18 19:26:57 +0800

    Removed beam and obsid uploading from upload_cands

--- scripts/upload_cands.py
+++ scripts/upload_cands.py
@@ -13,44 +13,14 @@ from vcstools.pointing_utils import sex2deg
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="""Upload the candidates on prometheus""")
+    parser = argparse.ArgumentParser(description="""Upload candidates to the SMART webapp""")
     parser.add_argument('-o', '--obsid', type=int,
             help="The observation ID of the MWA observation")
     parser.add_argument('-a', '--all', action='store_true',
             help="Load all PFD files in the current directory (overrides -p)")
-    parser.add_argument('-b', '--begin', type=int,
-            help="Begin time gps")
-    parser.add_argument('-e', '--end', type=int,
-            help="End time gps")
-    parser.add_argument('-f', '--file', type=str,
-            help="Pointing grid file")
-    parser.add_argument('-c', '--command', type=str,
-            help="mwa_search_pipeline.nf command")
-    parser.add_argument('-m', '--max_threads', type=int, default=8,
-            help="Maximum number of threads to use for concurrent submission.")
-    parser.add_argument('-s', '--super_computer_id', type=int, default=1,
-            help="Super computer ID. Ozstar: 1. Garrawarla: 2. SHAO: 3. Galaxy: 4. Magnus: 5. Default 1.")
     parser.add_argument('-p', '--pfd_file', type=str,
             help="The PFD file to be uploaded. This can be overridden by -a")
     args = parser.parse_args()
-
-    '''
-    print("Uploading obsid")
-    try:
-        upload_obsid(args.obsid)
-    except requests.exceptions.HTTPError:
-        print("Obsid already uploaded")
-    '''
-
-    if args.file:
-        print("Uploading beams")
-        pointing_list = np.loadtxt(args.file, dtype=str)
-        if pointing_list.ndim == 0:
-            pointing_list.shape = (1,)
-        upload_beam(pointing_list, args.obsid, args.begin, args.end,
-                    mwa_search_command=args.command, mwa_search_version='v3.0', supercomputer_id=args.super_computer_id)
-    else:
-        print("WARNING: Not uploading beams")
 
     if args.pfd_file is None and args.all == False:
         raise RuntimeError("Either -p or -a must be supplied")
